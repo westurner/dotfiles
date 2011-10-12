dd9fddff89bf1ab0da42fe3e4b4a9ae0ea2b4347
Wes Turner <wes@wrd.nu>
2011-10-12 18:32:12 -0500

    Importing dotfiles packaging

---
+++ setup.py
@@ -0,0 +1,10 @@
+try:
+    import paver.tasks
+except ImportError:
+    import os
+    if os.path.exists("paver-minilib.zip"):
+        import sys
+        sys.path.insert(0, "paver-minilib.zip")
+    import paver.tasks
+
+paver.tasks.main()
