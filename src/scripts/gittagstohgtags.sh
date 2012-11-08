c7d276856a90f24d1a3904e9b217c3f5cb54f30d
Wes Turner <wes@wrd.nu>
2012-11-08 07:42:19 -0600

    Adding gittagstohgtags.sh script

---
+++ src/scripts/gittagstohgtags.sh
@@ -0,0 +1,37 @@
+#!/bin/bash 
+
+# hg-git workaround
+# lookup corresponding .hg rev ids for hg-git mapped revs
+# output suitable for .hgtags or .hg/localtags
+
+# usage:
+usage() {
+    echo "$0: hg-git tags"
+    echo ""
+    echo "usage: $0 <path>"
+    echo "ex: $0 . >> ./.hg/localtags"
+}
+
+if [ "$1" == "-h" ]; then
+    usage
+    exit 0
+fi
+
+repo=$1
+
+if [ -d "${repo}/.hg/git" ]; then
+    repodir="${repo}/.hg/git"
+elif [ -d "${repo}/.git" ]; then
+    repodir="${repo}/.git"
+else
+    echo "Repository "${repo}" not specified or not found"
+    exit -1
+fi
+
+gittags="${repodir}/refs/tags"
+
+for f in `ls "${gittags}"`; do
+    githsh=`cat ${gittags}/${f}`;
+    hghsh=`grep ${githsh} ${repo}/.hg/git-mapfile | cut -f2 -d' '`;
+    echo ${hghsh} ${f};
+done
