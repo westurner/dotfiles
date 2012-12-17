bba4dd15bebfde12d6c47a1d1c62a4ecf1dab3b3
Wes Turner <wes@wrd.nu>
2012-12-16 22:20:26 -0600

    Updated deb_search.py: expanded subprocess pattern

--- src/dotfiles/deb_search.py
+++ src/dotfiles/deb_search.py
@@ -1,20 +1,47 @@
 #!/usr/bin/python
-from subprocess import Popen as popen
+from __future__ import print_function
+
+from itertools import imap
+from itertools import tee
 from subprocess import PIPE
+from subprocess import Popen
 import sys
 
-def eachline(input, cmd):
-	map(cmd, input)
+def apt_show(package):
+    cmd = "apt-cache show %s" % package
+    return Popen(
+        cmd,
+        stdout=PIPE,
+        shell=True).stdout.read()
 
-def searchfor(name):
-	eachline(popen("apt-cache search %s" % name,stdout=PIPE,shell=True).stdout.readlines(),apt_search)
 
-def apt_search(line):
-	print "======="
-	print ''.join(popen("apt-cache show %s" % line.split(' ')[0],stdout=PIPE,shell=True).stdout.readlines())
+def apt_search(query):
+    cmd = "apt-cache search %s" % query
+    process = Popen(
+        cmd,
+        stdout=PIPE,
+        shell=True
+    )
+    return iter(process.stdout)
+
+
+def searchfor(query):
+    for line in apt_search(query):
+        pkg = line.split(' ',1)[0]
+        yield (pkg, apt_show(pkg))
+
+
+def main():
+    _output = sys.stdout
+    query = ' '.join(sys.argv[1:])
+    names, descs = tee(searchfor(query))
+    print(u"## Packages for query: %r" % query)
+    for pkg, desc in names:
+        print(u"# %s" % pkg, file=_output)
+    for pkg, desc in descs:
+        print(desc, file=_output)
 
 
 if __name__=="__main__":
-	searchfor(' '.join(sys.argv[1:]))
+    main()
 
-	
