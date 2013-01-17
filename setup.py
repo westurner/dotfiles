62bf6dc783a3aa516315c8d69cd66748f2b562d7
Wes Turner <wes@wrd.nu>
2013-01-16 19:34:38 -0600

    Updated setup.py: Added documentation

--- setup.py
+++ setup.py
@@ -1,4 +1,18 @@
+#!/usr/bin/env python
+"""
+dotfiles setup.py
+===================
+If ``paver.tasks`` does not exist,
+``setup.py`` loads ``paver-minilib.zip``,
+which contains a minimal version of `paver`_
+containing ``paver.tasks``.
 
+``paver.tasks`` enumerates and executes tasks defined in ``pavement.py``.
+
+see: ``pavement.py``.
+
+.. _paver: http://pypi.python.org/pypi/paver
+"""
 
 try:
     import paver.tasks
