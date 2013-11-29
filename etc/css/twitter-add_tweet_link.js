21e9ce0835609804d4cea73fc574a422af197966
Wes Turner <wes@wrd.nu>
2013-11-29 06:27:21 -0600

    Updated twitter-add_tweet_link.js to point to https://twitter.com/intent/tweet

--- etc/css/twitter-add_tweet_link.js
+++ etc/css/twitter-add_tweet_link.js
@@ -1,7 +1,7 @@
-/* add a 'share' link */
-var hdr = document.getElementById('content-main-heading');
-var link = document.createElement('a');
-link.setAttribute('href', 'https://twitter.com/share');
-var text = document.createTextNode('share');
+/* add a 'tweet' link */
+var hdr = document.getElementById("content-main-heading");
+var link = document.createElement("a");
+link.setAttribute("href", "https://twitter.com/intent/tweet");
+var text = document.createTextNode("tweet");
 link.appendChild(text);
 hdr.parentNode.appendChild(link);
