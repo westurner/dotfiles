89329806e4cd0f05cd8fecc6b60a4330b17c8ba1
Wes Turner <wes@wrd.nu>
2013-12-03 07:23:04 -0600

    Updated setup_f.lux.sh for OSX

--- scripts/setup_f.lux.sh
+++ scripts/setup_f.lux.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
-sudo add-apt-repository ppa:kilian/f.lux
-sudo apt-get update
-sudo apt-get install fluxgui
+
+if [ -d '/etc/apt' ]; then
+    sudo add-apt-repository ppa:kilian/f.lux
+    sudo apt-get update
+    sudo apt-get install fluxgui
+fi
+if [ -d '/Applications' ]; then
+    # http://justgetflux.com/dlmac.html
+    URL='https://justgetflux.com/mac/Flux.zip'
+    curl -fsSL "${URL}" > Flux.zip && \
+    unzip Flux.zip && \
+    sudo cp -Rf Flux.app /Applications/ && \
+    rm -rf Flux.app Flux.zip
+fi
 
