73e5562a17457c2312157c315c68b7ce3f4a7a8f
Wes Turner <wes@wrd.nu>
2014-05-20 12:34:50 -0500

    DOC: update dotfiles_status sh function

--- etc/bash/05-bashrc.dotfiles.sh
+++ etc/bash/05-bashrc.dotfiles.sh
@@ -1,22 +1,24 @@
 
 dotfiles_status() {
-    echo "#  === dotfiles config ==="
-    echo "PATH=${PATH}"
+    echo "### dotfiles_status()"
     echo "## path"
-    lightpath | sed 's/^\(.*\)/#  \1/g'
-    echo "#  --- virtualenv ---"
+    echo "PATH=${PATH}"
+    #lightpath | sed 's/^\(.*\)/#  \1/g'
+    echo "## virtualenv"
     echo "VIRTUAL_ENV=${VIRTUAL_ENV}"
     echo "## virtualenvwrapper"
     echo "PROJECT_HOME=${PROJECT_HOME}"
     echo "WORKON_HOME=${WORKON_HOME}"
     echo "## venv"
+    echo "_SRC=${_SRC}"
+    echo "_WRD=${_WRD}"
     echo "VIRTUAL_ENV_NAME=${VIRTUAL_ENV_NAME}"
     echo "__DOTFILES=${__DOTFILES}"
     echo "__PROJECTS=${__PROJECTS}"
-    echo "_WRD=${_WRD}"
     echo "## TODO"
     echo "__SRC=${__SRC}"
     echo "_DOCSHTML=${_DOCSHTML}"
+    echo "_USRLOG=${_USRLOG}"
 }
 
 
