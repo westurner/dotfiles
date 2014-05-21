80433cf7e4083f87fc0bbbda97420cb7077b2c29
Wes Turner <wes@wrd.nu>
2014-05-20 22:39:19 -0500

    BUG: readline config should only load w/ bash

--- etc/bash/03-bashrc.readline.sh
+++ etc/bash/03-bashrc.readline.sh
@@ -3,15 +3,17 @@
 #  vi-mode: vi(m) keyboard shortcuts
 set -o vi
 
-#  .         -- insert last argument (command mode)
-bind -m vi-command ".":insert-last-argument
+if [ -n "$BASH_VERSION" ]; then
+    #  .         -- insert last argument (command mode)
+    bind -m vi-command ".":insert-last-argument
 
-## emulate default bash
-#  <ctrl> l  -- clear screen
-bind -m vi-insert "\C-l.":clear-screen
-#  <ctrl> a  -- move to beginning of line (^)
-bind -m vi-insert "\C-a.":beginning-of-line
-#  <ctrl> e  -- move to end of line ($)
-bind -m vi-insert "\C-e.":end-of-line
-#  <ctrl> w  -- delete last word
-bind -m vi-insert "\C-w.":backward-kill-word
+    ## emulate default bash
+    #  <ctrl> l  -- clear screen
+    bind -m vi-insert "\C-l.":clear-screen
+    #  <ctrl> a  -- move to beginning of line (^)
+    bind -m vi-insert "\C-a.":beginning-of-line
+    #  <ctrl> e  -- move to end of line ($)
+    bind -m vi-insert "\C-e.":end-of-line
+    #  <ctrl> w  -- delete last word
+    bind -m vi-insert "\C-w.":backward-kill-word
+fi
