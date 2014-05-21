72122f468605a9ba9350ce597ee0e42442521a48
Wes Turner <wes@wrd.nu>
2014-05-21 03:06:27 -0500

    TST,BUG,CLN: make compare_installed.py write to tempfile.mkdtemp and .rmtree in tearDown, PEP8

--- scripts/compare_installed.py
+++ scripts/compare_installed.py
@@ -28,35 +28,40 @@ comm -23 currentlyinstalled.txt defaultinstalled.txt
 import collections
 import subprocess
 import os
+import tempfile
 
-MANIFEST_URL="http://mirror.pnl.gov/releases/12.04/ubuntu-12.04.2-desktop-i386.manifest"
-def get_package_lists(manifest_url=MANIFEST_URL, cache=False):
+MANIFEST_URL = "http://mirror.pnl.gov/releases/12.04/ubuntu-12.04.2-desktop-i386.manifest"
+
+def ensure_file(command, filename, overwrite=False, shell=False):
+    print(command)
+    if overwrite or not os.path.exists(filename):
+        ret = subprocess.call(command, shell=shell)
+        if ret:
+            raise Exception(ret)
+
+
+def read_lines(filename):
+    with file(filename) as f:
+        for line in f:
+            _line = line.strip()
+            if _line:
+                yield _line
+
+
+def get_package_lists(manifest_url=MANIFEST_URL, cache=False, output_dir=None):
     """
 
     adapted from: http://unix.stackexchange.com/a/3624
     """
 
-    def ensure_file(command, filename, overwrite=False, shell=False):
-        print(command)
-        if overwrite or not os.path.exists(filename):
-            ret = subprocess.call(command, shell=shell)
-            if ret:
-                raise Exception(ret)
 
-    def read_lines(filename):
-        with file(filename) as f:
-            for line in f:
-                _line = line.strip()
-                if _line:
-                    yield _line
-
-    output1 = 'currentlyinstalled.txt'
+    output1 = os.path.join(output_dir, 'currentlyinstalled.txt')
     cmd1 = '''aptitude search '~i !~M' -F '%%p' | sort -u > %r''' % (
-            output1)
+        output1)
 
-    output2 = 'defaultinstalled.txt'
+    output2 = os.path.join(output_dir, 'defaultinstalled.txt')
     cmd2 = '''wget -qO - %r | cut -d" " -f1 | sort -u > %r''' % (
-            manifest_url, output2)
+        manifest_url, output2)
 
     if not cache or not all(os.path.exists(p) for p in (output1, output2)):
         ensure_file(cmd1, output1, shell=True)
@@ -64,6 +69,7 @@ def get_package_lists(manifest_url=MANIFEST_URL, cache=False):
 
     installed = list(read_lines(output1))
     default = list(read_lines(output2))
+
 
     return installed, default
 
@@ -76,9 +82,9 @@ def create_package_list(default, installed):
     # == comm -23
     also_installed = [x for x in installed if x not in default]
 
-    ### 'easiest' solution
-    ## print "apt-get remove -y %s" % (' '.join(uninstalled))
-    ## print "apt-get install -y %s" % (' '.join(also_installed))
+    # 'easiest' solution
+    # print "apt-get remove -y %s" % (' '.join(uninstalled))
+    # print "apt-get install -y %s" % (' '.join(also_installed))
 
     # >>> why isn't this good enough?
     # <<< why manually install dependencies that may change?
@@ -87,11 +93,11 @@ def create_package_list(default, installed):
     # <<< "i swear i didn't manually install [...]"
 
     # 'least technical debt' solution
-    #import apt_pkg
+    # import apt_pkg
     try:
         import apt
     except ImportError:
-        dist_packages='/usr/lib/python2.7/dist-packages/'
+        dist_packages = '/usr/lib/python2.7/dist-packages/'
         import sys
         import os
         import tempfile
@@ -107,27 +113,26 @@ def create_package_list(default, installed):
 
     apt_cache = apt.Cache()
 
-    #stack = collections.dequeue()
+    # stack = collections.dequeue()
     def visit_graph(apt_cache, pkgname, depends, visited):
         try:
             pkg = apt_cache[pkgname]
         except KeyError as e:
-            print(e) # TODO
+            print(e)  # TODO
             return
 
         for pkgset in pkg.installedDependencies:
             for pkg in pkgset:
-                depends[ pkg.name ].append(pkgname)
+                depends[pkg.name].append(pkgname)
                 if pkgname not in visited:
                     visited[pkgname] = True
                     visit_graph(apt_cache, pkg.name, depends, visited)
-                #stack.push( pkg['name'] )
+                # stack.push( pkg['name'] )
 
     depends = collections.defaultdict(list)
     visited = {}
     for pkgname in also_installed:
         visit_graph(apt_cache, pkgname, depends, visited)
-
 
     minimal = [x for x in also_installed if x not in depends]
 
@@ -140,16 +145,28 @@ def pkg_graph():
     """
     pass
 
-
 import unittest
+import shutil
+
+
 class Test_pkg_graph(unittest.TestCase):
+    def setUp(self):
+        self.output_dir = tempfile.mkdtemp(prefix='compare_paths_')
+
+    def tearDown(self):
+        shutil.rmtree(self.output_dir)
+
     def test_get_package_lists(self):
-        installed, default = get_package_lists(cache=True)
+        installed, default = get_package_lists(
+            cache=True,
+            output_dir=self.output_dir)
         assert isinstance(installed, list)
         assert isinstance(default, list)
 
     def test_pkg_graph(self):
-        installed, default = get_package_lists(cache=True)
+        installed, default = get_package_lists(
+            cache=True,
+            output_dir=self.output_dir)
         (minimal, also_installed, uninstalled) = (
             create_package_list(default, installed))
         for x in minimal:
@@ -158,7 +175,7 @@ class Test_pkg_graph(unittest.TestCase):
             print("als: %s" % x)
         for x in uninstalled:
             print("uni: %s" % x)
-        #raise Exception()
+        # raise Exception()
 
 
 def main():
@@ -168,14 +185,14 @@ def main():
     prs = optparse.OptionParser(usage="./%prog : args")
 
     prs.add_option('-v', '--verbose',
-                    dest='verbose',
-                    action='store_true',)
+                   dest='verbose',
+                   action='store_true',)
     prs.add_option('-q', '--quiet',
-                    dest='quiet',
-                    action='store_true',)
+                   dest='quiet',
+                   action='store_true',)
     prs.add_option('-t', '--test',
-                    dest='run_tests',
-                    action='store_true',)
+                   dest='run_tests',
+                   action='store_true',)
 
     (opts, args) = prs.parse_args()
 
