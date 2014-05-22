72d14976e11475b45562a874f3e4a9a056299603
Wes Turner <wes@wrd.nu>
2014-05-22 03:56:12 -0500

    BUG,ENH: xlck.sh: see xlck[.sh] -h

--- etc/xlck.sh
+++ etc/xlck.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-# Screensaver.sh
+# xlck.sh
 
+export XAUTOLOCK_PID="~/.xautolock.${DISPLAY}.pid"
 
 xlck() {
-    _THIS=$(readlink -e "$BASH_SOURCE")
-    echo $_THIS
-    bash $_THIS
+    _XLCK=$(readlink -e "$BASH_SOURCE")
+    echo $_XLCK
+    bash $_XLCK $@
 }
 
 _xlck_install () {
@@ -15,6 +16,8 @@ _xlck_install () {
 }
 
 _xlck_setup () {
+    export _XLCK=$(readlink -e "$BASH_SOURCE")
+
     #XSET=$(type 'xset' 2>/dev/null)
     if [ -n "$DISPLAY" ]; then
         xset +dpms
@@ -27,35 +30,19 @@ _xlck_setup () {
     fi
 }
 
-_xlck_i3lock () {
-    /usr/bin/i3lock -d -c 202020
-}
-
 _xlck_xlock () {
     /usr/bin/xlock -mode blank -bg black -fg white -lockdelay 3
 }
 
-_xlck_xautolock () {
-    _LOCK_BIN="/usr/bin/xlock"
-    _LOCK_DELAY="1"  # mins
-    _LOCK_CMD="$_LOCK_BIN -mode blank -bg black -fg white -lockdelay 3"
-    NOTIFY_CMD='/usr/bin/zenity --warning \
-        --title "Screensaver starts in 10s" \
-        --text "OK to cancel\n\nNOTE: Mouse Corners:\n⬉ to prevent\n⬋ to start"\
-        --timeout=10'
-    /usr/bin/xautolock \
-        -secure \
-        -detectsleep \
-        -time $_LOCK_DELAY \
-        -notify 10 \
-        -notifier "$NOTIFY_CMD" \
-        -corners -0+0 \
-        -cornerdelay 10 \
-        -cornerredelay 10 \
-        -locker "$_LOCK_CMD"
+_xlck_i3lock () {
+    /usr/bin/i3lock -d -c 202020
 }
 
-
+xlck_lock () {
+    # lock the current display
+    # note: this will be run before suspend to RAM and Disk.
+    _xlck_i3lock
+}
 
 _suspend_to_ram () {
     sudo bash -c 'echo mem > /sys/power/state'
@@ -68,12 +55,12 @@ _suspend_to_disk () {
 
 xlck_lock_suspend_ram () {
     sudo bash -c 'whoami'
-    _i3lock && _suspend_to_ram
+    xlck_lock && _suspend_to_ram
 }
 
 xlck_lock_suspend_disk () {
     sudo bash -c 'whoami'
-    _i3lock && _suspend_to_disk
+    xlck_lock && _suspend_to_disk
 }
 
 xlck_suspend_ram () {
@@ -84,44 +71,103 @@ xlck_suspend_disk () {
     xlck_lock_suspend_disk
 }
 
-xlck_lock () {
-    _xlock
-}
-
 xlck_start() {
+    echo "Starting xlck ..."
     if [ -n "$DISPLAY" ]; then
         _xlck_setup
-        _xlck_xautolock &
+        _xlck_xautolock & disown
+        echo "Started xlck ..."
+        sleep 1
+        xlck_status
     fi
 }
 
 xlck_stop() {
-    # TODO: store PID
-    # TODO: check $DISPLAY
-    #pkill -9 xlck.sh
-    pkill -9 -u $USER xlock
-    pkill -9 -u $USER -f /usr/bin/xautolock
+    echo "Stopping xlck ..."
+    xlck_xautolock_stop
+    echo "xlck stopped"
 }
 
-
 xlck_restart() {
-    xautolock -restart
-    #xlck_stop
-    #xlck_start
+    # xautolock -restart  # does not work with xautolock -secure
+    xlck_stop
+    xlck_start
+}
+
+xlck_pgrep_autolock_this_display() {
+    pids=$(pgrep xautolock)
+    if [ -n "$pids" ]; then
+        ps eww -p ${pids} \
+        | grep " DISPLAY=$DISPLAY" \
+        | awk '{ print $1 }'
+    fi
+}
+
+xlck_xautolock_status() {
+    echo "Checking autolock status where DISPLAY=$DISPLAY"
+    _xautolock_actual_pids=$(xlck_pgrep_autolock_this_display)
+    if [ -n "$_xautolock_actual_pids" ]; then
+        ps ufw -p $_xautolock_actual_pids
+    else
+        echo "no autolock processes found on this \$DISPLAY"
+    fi
+}
+
+xlck_xautolock_stop() {
+    echo "Stopping xautolock where DISPLAY=$DISPLAY ..."
+    _xautolock_actual_pids=$(xlck_pgrep_autolock_this_display)
+    if [ -n "$_xautolock_actual_pids" ]; then
+        kill -9 $_xautolock_actual_pids
+        echo "Stopped xautolock"
+    else
+        echo "xautolock not running"
+    fi
+    xlck_xautolock_status
 }
 
 xlck_status() {
-    #TODO
-    ps ufx | egrep 'xlock|xautolock' -C 7
-    ps -ufx -p $(ps eww | grep " DISPLAY=$DISPLAY" | pycut -f0)
+    echo "Checking xlck status..."
+
+    xlck_xautolock_status
+
 }
 
+xlck_status_all() {
+    _xlck_pgrep="pgrep 'xautolock|xlock|i3lock'"
+    _xlck_pids=$(pgrep 'xautolock|xlock|i3lock')
+    if [ -n "$_xlck_pids" ]; then
+        echo "_xlck_pids=${_xlck_pids}"
+        ps ufw -p $_xlck_pids
+    else
+        echo "\"${_xlck_pgrep}\" did not find any process ids" 
+    fi
+}
 
 xlck_status_this_display(){
-    pids=$(ps eww | grep " DISPLAY=$DISPLAY" | awk '{ print $1 }')
-    for p in $pids; do
-        ps fx $p
-    done
+    display=${1:-$DISPLAY}
+    ps ufx -p
+    _pids=$(ps eww | grep "DISPLAY=$display" | pyline 'w[0]')
+}
+
+_xlck_xautolock () {
+    _LOCK_DELAY=${1:-"1"}  # mins
+    _NOTIFY_DELAY=${2:-"10"}  # seconds
+    _LOCK_CMD="/bin/bash $_XLCK -L"
+    NOTIFY_CMD="/usr/bin/zenity --warning \
+        --title '${_NOTIFY_DELAY}s to screensaver' \
+        --text 'OK to cancel\n\nNOTE: Mouse Corners:\n⬉ to prevent\n⬋ to start'\
+        --timeout=${_NOTIFY_DELAY}"
+    /usr/bin/xautolock \
+        -secure \
+        -detectsleep \
+        -time $_LOCK_DELAY \
+        -notify ${_NOTIFY_DELAY} \
+        -notifier "$NOTIFY_CMD" \
+        -corners -0+0 \
+        -cornerdelay ${_NOTIFY_DELAY} \
+        -cornerredelay ${_NOTIFY_DELAY} \
+        -locker "$_LOCK_CMD" \
+        -nowlocker "$_LOCK_CMD"
 }
 
 xlck_usage() {
@@ -141,8 +187,11 @@ xlck_usage() {
 }
 
 xlck_main () {
-    while getopts "SPRMDLXh" opt; do
+    while getopts "USPRMDLXh" opt; do
         case "${opt}" in
+            U)
+                xlck_status;
+                ;;
             S)
                 xlck_start;
                 ;;
