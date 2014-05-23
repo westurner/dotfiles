ea01621eeeb0c099041a0a438329e318661c75f1
Wes Turner <wes@wrd.nu>
2014-05-23 05:05:49 -0500

    DOC: swap USER/HOSTNAME in dotfiles_status()

--- etc/bash/05-bashrc.dotfiles.sh
+++ etc/bash/05-bashrc.dotfiles.sh
@@ -1,25 +1,25 @@
 
 dotfiles_status() {
-    echo "## dotfiles_status()"
+    #echo "## dotfiles_status()"
     #lightpath | sed 's/^\(.*\)/#  \1/g'
-    echo "HOSTNAME=${HOSTNAME}"
-    echo "USER=${USER}"
-    echo "VIRTUAL_ENV_NAME=${VIRTUAL_ENV_NAME}"
-    echo "VIRTUAL_ENV=${VIRTUAL_ENV}"
-    echo "_SRC=${_SRC}"
-    echo "_WRD=${_WRD}"
-    echo "_USRLOG=${_USRLOG}"
-    echo "__DOTFILES=${__DOTFILES}"
-    echo "__DOCSWWW=${_DOCS}"
-    echo "__SRC=${__SRC}"
-    echo "__PROJECTSRC=${__PROJECTSRC}"
-    echo "PROJECT_HOME=${PROJECT_HOME}"
-    echo "WORKON_HOME=${WORKON_HOME}"
-    echo "PATH=${PATH}"
+    echo "USER='${USER}'"
+    echo "HOSTNAME='${HOSTNAME}'"
+    echo "VIRTUAL_ENV_NAME='${VIRTUAL_ENV_NAME}'"
+    echo "VIRTUAL_ENV='${VIRTUAL_ENV}'"
+    echo "_SRC='${_SRC}'"
+    echo "_WRD='${_WRD}'"
+    echo "_USRLOG='${_USRLOG}'"
+    echo "__DOTFILES='${__DOTFILES}'"
+    echo "__DOCSWWW='${_DOCS}'"
+    echo "__SRC='${__SRC}'"
+    echo "__PROJECTSRC='${__PROJECTSRC}'"
+    echo "PROJECT_HOME='${PROJECT_HOME}'"
+    echo "WORKON_HOME='${WORKON_HOME}'"
+    echo "PATH='${PATH}'"
 }
 
 reload_dotfiles() {
-    dotfiles_status $@
+    dotfiles_reload $@
 }
 
 if [ -d "${__DOTFILES}" ]; then
