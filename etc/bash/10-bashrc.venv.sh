9aa1d040498823d781330fd9497654be330ae780
Wes Turner <wes@wrd.nu>
2014-10-25 11:37:58 -0500

    CLN: == to compare

--- etc/bash/10-bashrc.venv.sh
+++ etc/bash/10-bashrc.venv.sh
@@ -234,7 +234,7 @@ _set_prompt() {
     fi
 
     if [ -n "$BASH_VERSION" ]; then
-        if [ "$color_prompt" = yes ]; then
+        if [ "$color_prompt" == yes ]; then
             PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\n\$ '
         else
             PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\n\$ '
