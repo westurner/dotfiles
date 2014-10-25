3fc81c17165e0e02c014514cb2f97ea58c7efe11
Wes Turner <wes@wrd.nu>
2014-10-25 11:51:56 -0500

    BUG: fix lightpath, lspath on OSX (where sed cannot substitute in a newline)

--- etc/bash/01-bashrc.lib.sh
+++ etc/bash/01-bashrc.lib.sh
@@ -6,6 +6,7 @@ function_exists() {
 
 add_to_path ()
 {
+    #  add_to_path  -- prepend a directory to $PATH
     ## http://superuser.com/questions/ \
     ##   39751/add-directory-to-path-if-its-not-already-there/39840#39840
 
@@ -24,16 +25,18 @@ add_to_path ()
 lightpath() {
     #  lightpath    -- display $PATH with newlines
     echo ''
-    echo $PATH | sed 's/\:/\n/g'
+    echo $PATH | tr ':' '\n'
 }
 
 lspath() {
     #  lspath       -- list files in each directory in $PATH
     echo "PATH=$PATH"
     lightpath
+    LS_OPTS=${@:'-ald'}
+    # LS_OPTS="-aldZ"
     for f in $(lightpath); do
         echo "# $f";
-        ls -aldZ $@ $f/*;
+        ls $LS_OPTS $@ $f/*;
     done
 }
 
