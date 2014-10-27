f524f883c673e9aa4c39bc78dd5d2b5de80e5cf2
Wes Turner <wes@wrd.nu>
2014-10-27 06:54:30 -0500

    DOC: scripts/dotfiles-i3.sh scripts/dotfiles-vim.sh remove leading spaces

--- scripts/dotfiles-vim.sh
+++ scripts/dotfiles-vim.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
 print_dotvim_comments() {
-    echo ""
-    echo '::' 
-    echo ""
     (cd $__DOTFILES;
-
     for f in $(ls etc/vim/vimrc*); do
         echo "   # $f";
-        cat $f | pyline -r '^\s*"\s(\s*.*)' 'rgx and "   " + rgx.group(1)';
+        cat $f | pyline -r '^\s*"\s(\s*.*)' 'rgx and l';
         echo "   ";
         echo "   ";
     done)
