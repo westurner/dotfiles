dde572bd76235f0e5a63d66b51380398d565f32e
Wes Turner <wes@wrd.nu>
2014-10-27 00:32:50 -0500

    DOC: Add docs to 00-bashrc.before.sh

--- etc/bash/00-bashrc.before.sh
+++ etc/bash/00-bashrc.before.sh
@@ -1,98 +1,105 @@
 
+#source ~/.bashrc
+  #source 00-bashrc.before.sh  # <-- THIS FILE
 
 dotfiles_reload() {
+    ## dotfiles_reload()    -- (re)load the bash configuration tree
+    #                          from ${__DOTFILES}/etc/bash
+    #  __DOTFILES (str)     -- path to this dotfiles repository (~/.dotfiles)
+
     echo "## Reloading bash configuration..."
     conf=${__DOTFILES}/etc/bash
-  #source ~/.bashrc
-    #source 00-bashrc.before.sh
 
-      ## libraries: useful bash functions
+      ## 01-bashrc.lib.sh  -- libraries: useful bash functions
       source ${conf}/01-bashrc.lib.sh
+      detect_platform
+      #  detect_platform() -- set __IS_MAC, __IS_LINUX vars [01-bashrc.lib.sh]
+      #                       egrep -nr -C 3 '__IS_MAC|__IS_LINUX'
 
-      ## readline
+      ## 03-bashrc.readline.sh  -- readline config
       source ${conf}/03-bashrc.readline.sh
 
-      ## TERM
+      ## 04-bashrc.TERM.sh      -- TERM, CLICOLOR
       source ${conf}/04-bashrc.TERM.sh
 
-      ## dotfiles
+      ## 05-bashrc.dotfiles.sh  -- dotfiles
       #  $__DOTFILES (str): path to local dotfiles repository clone
+      #  dotfiles_status(): print dotfiles env config
       export __DOTFILES="${HOME}/.dotfiles"
       source ${conf}/05-bashrc.dotfiles.sh
 
 
-      ## python: python: pip, virtualenv, virtualenvwrapper
-      #  TODO: PYTHON_
+      ### python: python: pip, virtualenv, virtualenvwrapper
       #  $PROJECT_HOME (str): path to project directory (~/wrk)
-      #  $WORKON_HOME (str): path to virtualenvs directory (~/wrk/.ve)
-      #  $VIRTUAL_ENV (str): path to current $VIRTUAL_ENV
+      #  $WORKON_HOME  (str): path to virtualenvs directory (~/wrk/.ve)
+      #  $VIRTUAL_ENV  (str): path to current $VIRTUAL_ENV
+
+      ## 07-bashrc.python.sh            -- python
       source ${conf}/07-bashrc.python.sh
+      ## 07-bashrc.virtualenv.sh        -- virtualenv
       source ${conf}/07-bashrc.virtualenv.sh
+      ## 07-bashrc.virtualenvwrapper.sh -- virtualenvwrapper
       source ${conf}/07-bashrc.virtualenvwrapper.sh
 
-      ## gcloud: Google Cloud SDK
+      ## 08-bashrc.gcloud.sh    -- gcloud: Google Cloud SDK
       #  _setup_google_cloud
       source ${conf}/08-bashrc.gcloud.sh
 
-      ## venv: virtualenvwrapper extensions (shell vars, cmds, aliases)
+      ## 10-bashrc.venv.sh      -- venv: virtualenvwrapper extensions
       #  $_VENVNAME (str): name of current $VIRTUAL_ENV
-      #  we() -- workon a new venv (virtualenvwrapper virtualenv + venv)
-      #          we() -> workon $1 && source <($_VENV --bash $@)
+      #  we() -- workon a new venv (source bin/activate; update ENVIRON)
+      #          we() -> workon $1 [$_APP] && source <($_VENV --bash $@)
       #          example::
-      #             we $venvname # $appname
-      if [ -d /Library ]; then
-          export __IS_MAC='true'
-      else
-          export __IS_LINUX='true'
-      fi
+      #             we dotfiles
+      #             we dotfiles etc/bash; ls -al; git status
       source ${conf}/10-bashrc.venv.sh
       # test -f $__PROJECTS && source $__PROJECTS
-      #  dotfiles_status() -- print dotfiles env config
       dotfiles_status
 
-      ## venv-pyramid: pyramid development workflow
-      #  workon_pyramid_add(venvname, appname)
+      ## 11-bashrc.venv.pyramid.sh  -- venv-pyramid: pyramid-specific config
+      #  workon_pyramid_app $VIRTUAL_ENV_NAME $_APP
       source ${conf}/11-bashrc.venv.pyramid.sh
 
 
-      ## editor/pager
-      #  $EDITOR (str): cmdstring to open $@ in current editor
-      #  $PAGER (str): cmdstring to run pager (less/vim)
-      #  lessv() -- open read only in vim
-      #  lessg() -- open read only in gvim/mvim
-      #  man()   -- open manpage in vim
-      #  mang()  -- open manpage in gvim/mvim
+      ## 20-bashrc.editor.sh        -- $EDITOR configuration
+      #  $_EDIT_ (str): cmdstring to open $@ (file list) in current editor
+      #  $EDITOR (str): cmdstring to open $@ (file list) in current editor
       source ${conf}/20-bashrc.editor.sh
+      ## 20-bashrc.vimpagers.sh     -- $PAGER configuration
+      #  $PAGER (str): cmdstring to run pager (less/vim)
       source ${conf}/29-bashrc.vimpagers.sh
 
 
-      ## usrlog:
+      ## 30-bashrc.usrlog.sh        -- $_USRLOG configuration
       #  $_USRLOG (str): path to .usrlog command log
       source ${conf}/30-bashrc.usrlog.sh
 
 
-      ## xlck: screensaver, screen (auto) lock, suspend
+      ## 30-bashrc.xlck.sh          -- screensaver, (auto) lock, suspend
       source ${conf}/30-bashrc.xlck.sh
 
 
-      ## aliases: bash aliases and cmds
+      ## 40-bashrc.aliases.sh       -- bash aliases and cmds
       source ${conf}/40-bashrc.aliases.sh
 
 
-      ## bashmarks: local bookmarks
+      ## 50-bashrc.bashmarks.sh     -- bashmarks: local bookmarks
       source ${conf}/50-bashrc.bashmarks.sh
 
-      ## repos: local repository set mgmt, docs hosting
+      ## 70-bashrc.repos.sh         -- repos: $__SRC repos, docs
       source ${conf}/70-bashrc.repos.sh
 
-    ## after: cleanup
+    ### 99-bashrc.after.sh          -- after: cleanup
     source ${conf}/99-bashrc.after.sh
 }
 
 dr() {
+    ## dr               -- dotfiles_reload
     dotfiles_reload $@
 }
 
+
+## dotfiles_reload()    -- called when source-ing in 00-bashrc.before.sh
 dotfiles_reload
 
 
