27d8b1e5b98352af4d0cea0327cdc64bab18110c
Wes Turner <wes@wrd.nu>
2014-10-27 01:42:29 -0500

    ENH: $_USRLOG: note(), usrlogv, usrlogg, usrloge

--- etc/bash/30-bashrc.usrlog.sh
+++ etc/bash/30-bashrc.usrlog.sh
@@ -1,7 +1,30 @@
 
 
 ## usrlog   -- Userspace shell logging
-#  stid -- set or regenerate shell session id
+## stid()   -- set or regenerate shell session id
 #  
 source "${__DOTFILES}/etc/usrlog.sh"
 _usrlog_setup
+
+note() {
+    ## note()   -- _usrlog_append # $@
+    _usrlog_append "## " $@
+}
+
+usrlogv() {
+    ## usrlog() -- open $_USRLOG with vim (skip to end)
+    file=${1:-$_USRLOG}
+    lessv + ${file}
+}
+
+usrlogg() {
+    ## usrlogv()    -- open $_USRLOG with gvim / mvim
+    file=${1:-$_USRLOG}
+    lessg + ${file}
+}
+
+usrloge() {
+    ## usrloge()    -- open $_USRLOG with venv's vim server
+    file=${1:-$_USRLOG}
+    lesse "+ ${file}"
+}
