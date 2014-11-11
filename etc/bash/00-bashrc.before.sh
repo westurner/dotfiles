e45c71432af760c8d42a5fcb73e0b9ef5a8cb297
Wes Turner <wes@wrd.nu>
2014-11-11 13:28:38 -0600

    DOC: 00-bashrc.before.sh: docstrings

--- etc/bash/00-bashrc.before.sh
+++ etc/bash/00-bashrc.before.sh
@@ -1,164 +1,157 @@
-
-#
-## 00-bashrc.before.sh
-#
-#### source ~/.bashrc
-###  > source etc/bash/*-bashrc.*.sh
-##     > source 00-bashrc.before.sh  # <-- THIS FILE
+#!/bin/bash
+## 00-bashrc.before.sh     -- bash dotfiles configuration root
+#  source ${__DOTFILES}/etc/bash/00-bashrc.before.sh    -- dotfiles_reload()
 #
 dotfiles_reload() {
-    ## dotfiles_reload()    -- (re)load the bash configuration tree
-    #                          from ${__DOTFILES}/etc/bash
-    #  __DOTFILES (str)     -- path to this dotfiles repository (~/.dotfiles)
+  #  dotfiles_reload()  -- (re)load the bash configuration
+  #  $__DOTFILES (str)  -- path to this dotfiles repository (~/.dotfiles)
 
-    echo "#"
-    echo "# dotfiles_reload()"
+  echo "#"
+  echo "# dotfiles_reload()"
 
-    if [ -n $__DOTFILES ]; then
-        export __DOTFILES=${__DOTFILES}
-    else
-        _dotfiles_src=${WORKON_HOME}/dotfiles/src/dotfiles
-        _dotfiles_link=${HOME}/.dotfiles
+  if [ -n $__DOTFILES ]; then
+  export __DOTFILES=${__DOTFILES}
+  else
+  _dotfiles_src=${WORKON_HOME}/dotfiles/src/dotfiles
+  _dotfiles_link=${HOME}/.dotfiles
 
-        if [ -d $_dotfiles_link ]; then
-            __DOTFILES=${_dotfiles_link}
-        elif [ -d $_dotfiles_src ]; then
-            __DOTFILES=${_dotfiles_src}
-        fi
-        export __DOTFILES=${__DOTFILES}
-    fi
+  if [ -d $_dotfiles_link ]; then
+  __DOTFILES=${_dotfiles_link}
+  elif [ -d $_dotfiles_src ]; then
+  __DOTFILES=${_dotfiles_src}
+  fi
+  export __DOTFILES=${__DOTFILES}
+  fi
 
-    conf=${__DOTFILES}/etc/bash
+  conf=${__DOTFILES}/etc/bash
 
-      #
-      ## 01-bashrc.lib.sh           -- useful bash functions (paths)
-      #  lspath()           -- list every file along $PATH
-      #  realpath()         -- readlink -f (python os.path.realpath)
-      #  walkpath()         -- list every directory along ${1:-"."}
-      source ${conf}/01-bashrc.lib.sh
+  #
+  ## 01-bashrc.lib.sh       -- useful bash functions (paths)
+  #  lspath()       -- list every file along $PATH
+  #  realpath()     -- readlink -f (python os.path.realpath)
+  #  walkpath()     -- list every directory along ${1:-"."}
+  source ${conf}/01-bashrc.lib.sh
 
-      #
-      ## 02-bashrc.platform.sh      -- platform things
-      source ${conf}/02-bashrc.platform.sh
-      detect_platform
-      #  detect_platform()  -- set __IS_MAC__IS_LINUX 
-      if [ -n "${__IS_MAC}" ]; then
-          export PATH=$(echo ${PATH} | sed 's,/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin,/usr/sbin:/sbin:/bin:/usr/local/bin:/usr/bin,')
-      fi
+  #
+  ## 02-bashrc.platform.sh  -- platform things
+  source ${conf}/02-bashrc.platform.sh
+  detect_platform
+  #  detect_platform()  -- set __IS_MAC__IS_LINUX 
+  if [ -n "${__IS_MAC}" ]; then
+      export PATH=$(echo ${PATH} | sed 's,/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin,/usr/sbin:/sbin:/bin:/usr/local/bin:/usr/bin,')
+  fi
 
-      #
-      ## 03-bashrc.readline.sh      -- readline
-      source ${conf}/03-bashrc.readline.sh
+  #
+  ## 03-bashrc.readline.sh  -- readline
+  source ${conf}/03-bashrc.readline.sh
 
-      #
-      ## 04-bashrc.TERM.sh          -- set $TERM and $CLICOLOR
-      source ${conf}/04-bashrc.TERM.sh
+  #
+  ## 04-bashrc.TERM.sh      -- set $TERM and $CLICOLOR
+  source ${conf}/04-bashrc.TERM.sh
 
-      #
-      ## 05-bashrc.dotfiles.sh      -- dotfiles
-      #  $__DOTFILES (str): path to local dotfiles repository clone
-      #  dotfiles_status(): print dotfiles env config
-      source ${conf}/05-bashrc.dotfiles.sh
-      dotfiles_add_path
+  #
+  ## 05-bashrc.dotfiles.sh  -- dotfiles
+  #  $__DOTFILES (str): path to local dotfiles repository clone
+  #  dotfiles_status(): print dotfiles env config
+  source ${conf}/05-bashrc.dotfiles.sh
+  dotfiles_add_path
 
-      #
-      ## 06-bashrc.completion.sh    -- configure bash completion
-      source ${conf}/06-bashrc.completion.sh
+  #
+  ## 06-bashrc.completion.sh    -- configure bash completion
+  source ${conf}/06-bashrc.completion.sh
 
-      #
-      #  python: pip, virtualenv, virtualenvwrapper
-      #  $PROJECT_HOME (str): path to project directory (~/wrk)
-      #  $WORKON_HOME  (str): path to virtualenvs directory (~/wrk/.ve)
-      #  $VIRTUAL_ENV  (str): path to current $VIRTUAL_ENV
+  #
+  #  python: pip, virtualenv, virtualenvwrapper
+  #  $PROJECT_HOME (str): path to project directory (~/wrk)
+  #  $WORKON_HOME  (str): path to virtualenvs directory (~/wrk/.ve)
+  #  $VIRTUAL_ENV  (str): path to current $VIRTUAL_ENV
 
-      #
-      ## 07-bashrc.python.sh        -- python
-      #  _setup_anaconda()      -- setup anaconda paths (manual)
-      #  _setup_pyenv()         -- setup pyenv paths (manual)
-      source ${conf}/07-bashrc.python.sh
+  #
+  ## 07-bashrc.python.sh    -- python
+  #  _setup_anaconda()  -- setup anaconda paths (manual)
+  #  _setup_pyenv()     -- setup pyenv paths (manual)
+  source ${conf}/07-bashrc.python.sh
 
-      #
-      ## 07-bashrc.virtualenv.sh    -- virtualenv
-      source ${conf}/07-bashrc.virtualenv.sh
+  #
+  ## 07-bashrc.virtualenv.sh    -- virtualenv
+  source ${conf}/07-bashrc.virtualenv.sh
 
-      #
-      ## 07-bashrc.virtualenvwrapper.sh -- virtualenvwrapper
-      source ${conf}/07-bashrc.virtualenvwrapper.sh
+  #
+  ## 07-bashrc.virtualenvwrapper.sh -- virtualenvwrapper
+  source ${conf}/07-bashrc.virtualenvwrapper.sh
 
+  #
+  ## 08-bashrc.gcloud.sh    -- gcloud: Google Cloud SDK
+  #  _setup_google_cloud()  -- setup google cloud paths
+  source ${conf}/08-bashrc.gcloud.sh
 
-      #
-      ## 08-bashrc.gcloud.sh        -- gcloud: Google Cloud SDK
-      #  _setup_google_cloud()  -- setup google cloud paths
-      source ${conf}/08-bashrc.gcloud.sh
+  #
+  ## 10-bashrc.venv.sh      -- venv: virtualenvwrapper extensions
+  #  $_VENVNAME (str): name of current $VIRTUAL_ENV
+  #  we() -- workon a new venv (source bin/activate; update ENVIRON)
+  #      we() -> workon $1 [$_APP] && source <($_VENV --bash $@)
+  #      example::
+  #     we dotfiles
+  #     we dotfiles etc/bash; cdw; ls
+  source ${conf}/10-bashrc.venv.sh
+  #  $__PROJECTS (str): local script to source
+  #  dotfiles_status()  -- print dotfiles variables
+  #  ds()       -- print dotfiles variables
 
-      #
-      ## 10-bashrc.venv.sh          -- venv: virtualenvwrapper extensions
-      #  $_VENVNAME (str): name of current $VIRTUAL_ENV
-      #  we() -- workon a new venv (source bin/activate; update ENVIRON)
-      #          we() -> workon $1 [$_APP] && source <($_VENV --bash $@)
-      #          example::
-      #             we dotfiles
-      #             we dotfiles etc/bash; cdw; ls
-      source ${conf}/10-bashrc.venv.sh
-      #  $__PROJECTS (str): local script to source
-      #  dotfiles_status()      -- print dotfiles variables
-      #  ds()                   -- print dotfiles variables
+  #
+  ## 11-bashrc.venv.pyramid.sh  -- venv-pyramid: pyramid-specific config
+  source ${conf}/11-bashrc.venv.pyramid.sh
 
-      #
-      ## 11-bashrc.venv.pyramid.sh  -- venv-pyramid: pyramid-specific config
-      source ${conf}/11-bashrc.venv.pyramid.sh
+  #
+  ## 20-bashrc.editor.sh    -- $EDITOR configuration
+  #  $_EDIT_ (str): cmdstring to open $@ (file list) in current editor
+  #  $EDITOR (str): cmdstring to open $@ (file list) in current editor
+  source ${conf}/20-bashrc.editor.sh
+  ## 20-bashrc.vimpagers.sh     -- $PAGER configuration
+  #  $PAGER (str): cmdstring to run pager (less/vim)
+  source ${conf}/29-bashrc.vimpagers.sh
 
-      #
-      ## 20-bashrc.editor.sh        -- $EDITOR configuration
-      #  $_EDIT_ (str): cmdstring to open $@ (file list) in current editor
-      #  $EDITOR (str): cmdstring to open $@ (file list) in current editor
-      source ${conf}/20-bashrc.editor.sh
-      ## 20-bashrc.vimpagers.sh     -- $PAGER configuration
-      #  $PAGER (str): cmdstring to run pager (less/vim)
-      source ${conf}/29-bashrc.vimpagers.sh
-
-
-      #
-      ## 30-bashrc.usrlog.sh        -- $_USRLOG configuration
-      #  $_USRLOG (str): path to .usrlog command log
-      #  stid           -- set $TERM_ID to a random string
-      #  stid $name     -- set $TERM_ID to string
-      #  note           -- add a dated note to $_USRLOG [_usrlog_append]
-      #  usrlogv        -- open usrlog with vim:   $VIMBIN + $_USRLOG
-      #  usrlogg        -- open usrlog with gmvim: $GUIVIMBIN + $_USRLOG
-      #  usrloge        -- open usrlog with editor:$EDITOR + $_USRLOG
-      #  ut             -- tail $_USRLOG
-      #  ug             -- egrep current usrlog: egrep $@ $_USRLOG
-      #  ugall          -- egrep $@ $__USRLOG ${WORKON_HOME}/*/.usrlog
-      #  ugrin          -- grin current usrlog: grin $@ $_USRLOG
-      #  ugrinall       -- grin $@  $__USRLOG ${WORKON_HOME}/*/.usrlog
-      #  lsusrlogs      -- ls -tr   $__USRLOG ${WORKON_HOME}/*/.usrlog
-      source ${conf}/30-bashrc.usrlog.sh
+  #
+  ## 30-bashrc.usrlog.sh    -- $_USRLOG configuration
+  #  $_USRLOG (str): path to .usrlog command log
+  #  stid       -- set $TERM_ID to a random string
+  #  stid $name     -- set $TERM_ID to string
+  #  note       -- add a dated note to $_USRLOG [_usrlog_append]
+  #  usrlogv    -- open usrlog with vim:   $VIMBIN + $_USRLOG
+  #  usrlogg    -- open usrlog with gmvim: $GUIVIMBIN + $_USRLOG
+  #  usrloge    -- open usrlog with editor:$EDITOR + $_USRLOG
+  #  ut     -- tail $_USRLOG
+  #  ug     -- egrep current usrlog: egrep $@ $_USRLOG
+  #  ugall      -- egrep $@ $__USRLOG ${WORKON_HOME}/*/.usrlog
+  #  ugrin      -- grin current usrlog: grin $@ $_USRLOG
+  #  ugrinall   -- grin $@  $__USRLOG ${WORKON_HOME}/*/.usrlog
+  #  lsusrlogs  -- ls -tr   $__USRLOG ${WORKON_HOME}/*/.usrlog
+  source ${conf}/30-bashrc.usrlog.sh
 
 
-      ## 30-bashrc.xlck.sh          -- screensaver, (auto) lock, suspend
-      source ${conf}/30-bashrc.xlck.sh
+  ## 30-bashrc.xlck.sh      -- screensaver, (auto) lock, suspend
+  source ${conf}/30-bashrc.xlck.sh
 
 
-      ## 40-bashrc.aliases.sh       -- aliases
-      source ${conf}/40-bashrc.aliases.sh
-      ## 42-bashrc.commands.sh      -- example commands
-      source ${conf}/42-bashrc.commands.sh
+  ## 40-bashrc.aliases.sh   -- aliases
+  source ${conf}/40-bashrc.aliases.sh
+  ## 42-bashrc.commands.sh  -- example commands
+  source ${conf}/42-bashrc.commands.sh
 
 
-      ## 50-bashrc.bashmarks.sh     -- bashmarks: local bookmarks
-      source ${conf}/50-bashrc.bashmarks.sh
+  ## 50-bashrc.bashmarks.sh     -- bashmarks: local bookmarks
+  source ${conf}/50-bashrc.bashmarks.sh
 
 
-      ## 70-bashrc.repos.sh         -- repos: $__SRC repos, docs
-      source ${conf}/70-bashrc.repos.sh
+  ## 70-bashrc.repos.sh     -- repos: $__SRC repos, docs
+  source ${conf}/70-bashrc.repos.sh
 
-    ### 99-bashrc.after.sh          -- after: cleanup
-    source ${conf}/99-bashrc.after.sh
+### 99-bashrc.after.sh      -- after: cleanup
+  source ${conf}/99-bashrc.after.sh
 }
 
 dr() {
-    ## dr               -- dotfiles_reload
+    # dr()  -- dotfiles_reload
     dotfiles_reload $@
 }
 
