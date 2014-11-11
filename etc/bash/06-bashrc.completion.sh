68dd1106bceb2132034a1b51488d33c4e1d630e1
Wes Turner <wes@wrd.nu>
2014-11-11 15:17:45 -0600

    DOC,BUG: bashrc.completion.sh: check shopt -oq posix, integrate from ~/.bashrc

--- etc/bash/06-bashrc.completion.sh
+++ etc/bash/06-bashrc.completion.sh
@@ -1,9 +1,15 @@
 
+### bashrc.completion.sh
 
-_configure_completion() {
-    ## configure bash completion (`complete -p` to list completions)
+_configure_bash_completion() {
+    # _configure_bash_completion()  -- configure bash completion
+    #                               note: `complete -p` lists completions
+
+    if ! shopt -oq posix; then
+        return
+    fi
     if [ -n "$__IS_MAC" ]; then
-        # configure brew (brew install bash-completion)
+        #configure brew (brew install bash-completion)
         BREW=$(which brew 2>/dev/null || false)
         if [ -n "${BREW}" ]; then
             brew_prefix=$(brew --prefix)
@@ -11,6 +17,10 @@ _configure_completion() {
                 source ${brew_prefix}/etc/bash_completion
             fi
         fi
+    else
+        if [ -f /etc/bash_completion ]; then
+            source /etc/bash_completion
+        fi
     fi
 }
-_configure_completion
+_configure_bash_completion
