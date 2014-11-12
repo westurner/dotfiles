e3bf363a16100f2afe7aa09c138584842e4cc983
Wes Turner <wes@wrd.nu>
2014-11-11 20:47:27 -0600

    DOC: 30-bashrc.usrlog.sh: docstrings, _setup_usrlog

--- etc/bash/30-bashrc.usrlog.sh
+++ etc/bash/30-bashrc.usrlog.sh
@@ -1,25 +1,43 @@
 
+### bashrc.usrlog.sh
+    # _USRLOG (str): path to .usrlog userspace shell command log
+    # stid()     -- set $TERM_ID to a random string
+    # stid $name -- set $TERM_ID to string
+    # note()     -- add a dated note to $_USRLOG [_usrlog_append]
+    # usrlogv()  -- open usrlog with vim:   $VIMBIN + $_USRLOG
+    # usrlogg()  -- open usrlog with gmvim: $GUIVIMBIN + $_USRLOG
+    # usrloge()  -- open usrlog with editor:$EDITOR + $_USRLOG
+    # ut()       -- tail $_USRLOG
+    # ug()       -- egrep current usrlog: egrep $@ $_USRLOG
+    # ugall()    -- egrep $@ $__USRLOG ${WORKON_HOME}/*/.usrlog
+    # ugrin()    -- grin current usrlog: grin $@ $_USRLOG
+    # ugrinall() -- grin $@  $__USRLOG ${WORKON_HOME}/*/.usrlog
+    # lsusrlogs()-- ls -tr   $__USRLOG ${WORKON_HOME}/*/.usrlog
+    #
+    # source ${__DOTFILES}/etc/usrlog.sh
 
-## usrlog   -- Userspace shell logging
-## stid()   -- set or regenerate shell session id
-#  
-source "${__DOTFILES}/etc/usrlog.sh"
-_usrlog_setup
+_setup_usrlog() {
+    # _setup_usrlog()   -- source ${__DOTFILES}/etc/usrlog.sh
+    source "${__DOTFILES}/etc/usrlog.sh"
+    #calls _usrlog_setup when sourced
+}
+_setup_usrlog
+
 
 usrlogv() {
-    ## usrlog() -- open $_USRLOG with vim (skip to end)
+    # usrlogv() -- open $_USRLOG w/ $VIMBIN (and skip to end)
     file=${1:-$_USRLOG}
     lessv + ${file}
 }
 
 usrlogg() {
-    ## usrlogv()    -- open $_USRLOG with gvim / mvim
+    # usrlogg() -- open $_USRLOG w/ $GUIVIMBIN (and skip to end)
     file=${1:-$_USRLOG}
     lessg + ${file}
 }
 
 usrloge() {
-    ## usrloge()    -- open $_USRLOG with venv's vim server
+    # usrloge() -- open $_USRLOG w/ $EDITOR_ [ --servername $VIRTUAL_ENV_NAME ]
     file=${1:-$_USRLOG}
     lesse "+ ${file}"
 }
