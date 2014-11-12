81e9f72f392239c1eeb0d68ab402c15ab23fe29c
Wes Turner <wes@wrd.nu>
2014-11-11 21:20:59 -0600

    DOC: 30-bashrc.usrlog.sh: docstrings

--- etc/bash/30-bashrc.usrlog.sh
+++ etc/bash/30-bashrc.usrlog.sh
@@ -1,20 +1,18 @@
 
 ### bashrc.usrlog.sh
     # _USRLOG (str): path to .usrlog userspace shell command log
-    # stid()     -- set $TERM_ID to a random string
-    # stid $name -- set $TERM_ID to string
-    # note()     -- add a dated note to $_USRLOG [_usrlog_append]
-    # usrlogv()  -- open usrlog with vim:   $VIMBIN + $_USRLOG
-    # usrlogg()  -- open usrlog with gmvim: $GUIVIMBIN + $_USRLOG
-    # usrloge()  -- open usrlog with editor:$EDITOR + $_USRLOG
-    # ut()       -- tail $_USRLOG
-    # ug()       -- egrep current usrlog: egrep $@ $_USRLOG
-    # ugall()    -- egrep $@ $__USRLOG ${WORKON_HOME}/*/.usrlog
-    # ugrin()    -- grin current usrlog: grin $@ $_USRLOG
-    # ugrinall() -- grin $@  $__USRLOG ${WORKON_HOME}/*/.usrlog
-    # lsusrlogs()-- ls -tr   $__USRLOG ${WORKON_HOME}/*/.usrlog
-    #
-    # source ${__DOTFILES}/etc/usrlog.sh
+    # stid()      -- set $TERM_ID to a random string
+    # stid $name  -- set $TERM_ID to string
+    # note()      -- add a dated note to $_USRLOG [_usrlog_append]
+    # usrlogv()   -- open usrlog with vim:   $VIMBIN + $_USRLOG
+    # usrlogg()   -- open usrlog with gmvim: $GUIVIMBIN + $_USRLOG
+    # usrloge()   -- open usrlog with editor:$EDITOR + $_USRLOG
+    # ut()        -- tail $_USRLOG
+    # ug()        -- egrep current usrlog: egrep $@ $_USRLOG
+    # ugall()     -- egrep $@ $__USRLOG ${WORKON_HOME}/*/.usrlog
+    # ugrin()     -- grin current usrlog: grin $@ $_USRLOG
+    # ugrinall()  -- grin $@  $__USRLOG ${WORKON_HOME}/*/.usrlog
+    # lsusrlogs() -- ls -tr   $__USRLOG ${WORKON_HOME}/*/.usrlog
 
 _setup_usrlog() {
     # _setup_usrlog()   -- source ${__DOTFILES}/etc/usrlog.sh
--- etc/bash/30-bashrc.xlck.sh
+++ etc/bash/30-bashrc.xlck.sh
@@ -1,6 +1,13 @@
 
+### 30-bashrc.xlck.sh
+## xlck     -- minimal X screensaver
 
-## xlck     -- screensaver
-if [ ! -d '/Library' ]; then  # not on OSX
-    source "${__DOTFILES}/etc/xlck.sh"
-fi
+_setup_xlck() {
+    # _setup_xlck() -- source ${__DOTFILES}/etc/xlck.sh (if -z __IS_MAC)
+    if [ -z "${__IS_MAC}" ]; then
+        source "${__DOTFILES}/etc/xlck.sh"
+    fi
+}
+
+    # xlock_lock        -- lock the current display
+    # _xlck_xlck()      -- start xlock (white on black w/ a 3 second delay)
