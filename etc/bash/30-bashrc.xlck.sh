c963bb074d7ef0ac8b1676cac6b9781646d4721b
Wes Turner <wes@wrd.nu>
2014-11-11 21:30:11 -0600

    DOC: 30-bashrc.xlck.sh, xlck.sh: docstrings

--- etc/bash/30-bashrc.xlck.sh
+++ etc/bash/30-bashrc.xlck.sh
@@ -1,6 +1,19 @@
 
 ### 30-bashrc.xlck.sh
 ## xlck     -- minimal X screensaver
+    # xlck 
+    # xlck -I  --  (I)nstall xlck (apt-get)
+    # xlck -U  --  check stat(U)s (show xautolock processes on this $DISPLAY)
+    # xlck -S  --  (S)tart xlck (start xautolock on this $DISPLAY)
+    # xlck -P  --  sto(P) xlck (stop xautolock on this $DISPLAY)
+    # xlck -R  --  (R)estart xlck
+    # xlck -M  --  suspend to ra(M) (and lock)
+    # xlck -D  --  suspend to (D)isk (and lock)
+    # xlck -L  --  (L)ock
+    # xlck -X  --  shutdown -h now
+    # xlck -h  --  help
+    # xlck_status_all()             -- pgrep 'xautolock|xlock|i3lock', ps ufw
+    # xlck_status_this_display()    -- show status for this $DISPLAY
 
 _setup_xlck() {
     # _setup_xlck() -- source ${__DOTFILES}/etc/xlck.sh (if -z __IS_MAC)
@@ -9,5 +22,3 @@ _setup_xlck() {
     fi
 }
 
-    # xlock_lock        -- lock the current display
-    # _xlck_xlck()      -- start xlock (white on black w/ a 3 second delay)
