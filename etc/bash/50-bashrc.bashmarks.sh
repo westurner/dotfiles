e567e211cb247e138eeb6f57f64bc58ddfd0c599
Wes Turner <wes@wrd.nu>
2014-11-11 21:37:39 -0600

    DOC: 50-bashrc.bashmarks.sh: docstrings

--- etc/bash/50-bashrc.bashmarks.sh
+++ etc/bash/50-bashrc.bashmarks.sh
@@ -1,15 +1,17 @@
+
+### bashrc.bashmarks.sh
 ## bashmarks
-#  l    -- list bashmarks
-#  s    -- save bashmarks as $1
-#  g    -- goto bashmark $1
-#  p    -- print bashmark $1
-#  d    -- delete bashmark $1
+    # l()  -- list bashmarks
+    # s()  -- save bashmarks as $1
+    # g()  -- goto bashmark $1
+    # p()  -- print bashmark $1
+    # d()  -- delete bashmark $1
 source "${__DOTFILES}/etc/bashmarks/bashmarks.sh"
-#  lsbashmarks -- list Bashmarks (e.g. for NERDTree)
 lsbashmarks () {
+    # lsbashmarks() -- list Bashmarks (e.g. for NERDTree)
     export | grep 'DIR_' | pyline "line[15:].replace('\"','').split('=',1)"
 }
 
-# ~/.dotfiles/bin/nerdtree_to_bashmarks.py
+    # see also: ${__DOTFILES}/scripts/nerdtree_to_bashmarks.py
 
 
