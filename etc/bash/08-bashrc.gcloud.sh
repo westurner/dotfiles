590c4eb86f162f9e9d3812efc302636ab9a3d207
Wes Turner <wes@wrd.nu>
2014-11-11 19:56:03 -0600

    DOC: 08-bashrc.gcloud.sh: docstrings

--- etc/bash/08-bashrc.gcloud.sh
+++ etc/bash/08-bashrc.gcloud.sh
@@ -1,11 +1,13 @@
-_setup_google_cloud() {
-    _GCLOUD_PREFIX="/srv/wrk/google-cloud-sdk"
-    #  _setup_google_cloud  -- configure PATH and bash completions for
-    #   Google Cloud"
 
-    # The next line updates PATH for the Google Cloud SDK.
+### bashrc.gcloud.sh
+
+_setup_google_cloud() {
+    # _setup_google_cloud() -- configure gcloud $PATH and bash completions
+    export _GCLOUD_PREFIX="/srv/wrk/google-cloud-sdk"
+
+    #The next line updates PATH for the Google Cloud SDK.
     source "${_GCLOUD_PREFIX}/path.bash.inc"
 
-    # The next line enables bash completion for gcloud.
+    #The next line enables bash completion for gcloud.
     source "${_GCLOUD_PREFIX}/completion.bash.inc"
 }
