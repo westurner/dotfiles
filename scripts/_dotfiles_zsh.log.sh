64032bae723d304b85aeb610cd529ffe41e93b58
Wes Turner <wes@wrd.nu>
2015-04-14 22:25:55 -0500

    DOC: autogenerated usage docs

--- scripts/_dotfiles_zsh.log.sh
+++ scripts/_dotfiles_zsh.log.sh
@@ -4948,14 +4948,9 @@ elif [ -f "${_plugin__ssh_env}" ]; then
 else
   _plugin__start_agent;
 fi
-SSH_AUTH_SOCK=/var/folders/sh/m3c88fyn4x319133n00kjhg00000gn/T//ssh-gIcl0cPeCE26/agent.42058; export SSH_AUTH_SOCK;
-SSH_AGENT_PID=42060; export SSH_AGENT_PID;
-#echo Agent pid 42060;
 SSH_AUTH_SOCK=/var/folders/sh/m3c88fyn4x319133n00kjhg00000gn/T//ssh-H24lgXwSY4Yo/agent.26031; export SSH_AUTH_SOCK;
 SSH_AGENT_PID=26033; export SSH_AGENT_PID;
 #echo Agent pid 26033;
-starting ssh-agent...
-Identity added: /Users/W/.ssh/id_rsa (/Users/W/.ssh/id_rsa)
 
 # tidy up after ourselves
 unfunction _plugin__start_agent
