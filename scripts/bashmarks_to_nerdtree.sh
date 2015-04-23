517d23cd9582789524aa7af2adc75fa48c305f24
Wes Turner <wes@wrd.nu>
2015-04-23 12:38:10 -0500

    BUG: scripts/bashmarks_to_nerdtree.sh: pyline expression

--- scripts/bashmarks_to_nerdtree.sh
+++ scripts/bashmarks_to_nerdtree.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 # print bashmarks in nerdtree format
-export | grep 'DIR_' | pyline "line[15:].replace('\"','').split('=',1)"
+
+bashmarks_to_nerdtree() {
+    (set -x; _bashmarks_to_nerdtree $@)
+}
+
+_bashmarks_to_nerdtree() {
+    bookmarks="${1}"
+    PYLINE=$(which pyline)
+    PYLINE=${PYLINE:-"${__DOTFILES}/scripts/pyline.py"}
+    l
+    export | grep 'DIR_' | \
+        $PYLINE '" ".join(line.split(None, 2)[-1].lstrip("DIR_").rstrip().replace("\"","").split("=",1))' \
+        | (test -n "${bookmarks}" && tee "${bookmarks}" || cat)
+}
+
+bashmarks_to_nerdtree ${1:-${_NERDTREEBOOKMARKS:-"${HOME}/.NERDTreeBookmarks"}}
