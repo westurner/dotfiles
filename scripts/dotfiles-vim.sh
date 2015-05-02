8633b2b5ae251d5f4cc1557efcac42d790bf9ba0
Wes Turner <wes@wrd.nu>
2015-05-02 11:59:17 -0500

    REF,BLD: scripts/dotfiles-vim.sh: etc/vim -> etc/.vim

--- scripts/dotfiles-vim.sh
+++ scripts/dotfiles-vim.sh
@@ -2,7 +2,7 @@
 
 print_dotvim_comments() {
     (cd $__DOTFILES;
-    for f in $(ls etc/vim/vimrc*); do
+    for f in $(ls etc/.vim/vimrc*); do
         echo "   # $f";
         cat $f | pyline.py -r '^\s*"\s(\s*.*)' 'rgx and l';
         echo "   ";
