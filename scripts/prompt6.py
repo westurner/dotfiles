19e5dc2352f9b92e52110283c724e717c451bafd
Wes Turner <wes@wrd.nu>
2015-05-03 11:16:16 -0500

    ENH: prompt6.py, this.sh: POC

--- scripts/prompt6.py
+++ scripts/prompt6.py
@@ -27,8 +27,9 @@ import logging
 import os
 import subprocess
 
-# logging.basicConfig(level=logging.DEBUG)
-log = logging.getLogger()
+LOG_TRACE = 6
+logging.addLevelName('TRACE', LOG_TRACE)
+log = logging.getLogger(__name__)
 
 try:
     # import keyring (if it is available)
@@ -36,7 +37,7 @@ try:
     keyring
     from keyring.backend import KeyringBackend
 except Exception as e:
-    log.debug(e)
+    log.exception(e)
 
     class KeyringBackend(object):
         pass
@@ -55,6 +56,8 @@ class KeyValueStore(Store):  # , KeyringBackend):
     KEYDELIM = u".."
     KEYDELIM_ESCAPE = u"\.."
 
+    DEFAULT_PREFIX = '-p6'
+
     def __init__(self, prefix, data=None, conf=None):
         self.conf = collections.OrderedDict() if conf is None else conf
         self.set_prefix(prefix)
@@ -64,7 +67,8 @@ class KeyValueStore(Store):  # , KeyringBackend):
 
     def set_prefix(self, prefix=None):
         if prefix is None:
-            prefix = os.path.expanduser(os.path.expandvars(self.DEFAULT_PREFIX))
+            prefix = self.DEFAULT_PREFIX
+        prefix = os.path.expanduser(os.path.expandvars(prefix))
         self.conf['prefix'] = prefix
 
     def escape_key_part(self, keystr):
@@ -75,6 +79,8 @@ class KeyValueStore(Store):  # , KeyringBackend):
     def joinkey(self, args):
         parts = (self.escape_key_part(p) for p in args)
         return self.KEYDELIM.join(parts)
+
+    # [parts[0]] + [_p.lstrip(os.path.sep) for _p in parts[1:]])
 
     def path_to_key(self, path):
         return str(path).replace(os.path.sep, self.KEYDELIM)
@@ -94,7 +100,11 @@ class KeyValueStore(Store):  # , KeyringBackend):
 
     def __contains__(self, key):
         key = self.transform_key(key)
-        return key in self.data
+        log.log(LOG_TRACE, ' ? contains(%r) <%r>', key, self)
+        retval = key in self.data
+        if retval:
+            log.log(LOG_TRACE, ' X contains(%r) <%r>', key, self)
+
 
     def get(self, key, default=DEFAULT):
         key = self.transform_key(key)
@@ -108,10 +118,11 @@ class KeyValueStore(Store):  # , KeyringBackend):
         return self.get(key, default=None)
 
     def __getitem__(self, key):
+        log.log(LOG_TRACE, 'getitem: %r', key)
         return self.get(key)
 
     def __setitem__(self, key, value):
-        log.debug('setitem: %r, %r', key, value)
+        log.log(LOG_TRACE, 'setitem: %r, %r', key, value)
         # key = self.transform_key(key)
         # log.debug('transform_key: %r, %r', key, value)
         self.set(key, value)
@@ -137,11 +148,13 @@ class OnDiskKeyValueStore(KeyValueStore):
         self.set_prefix(prefix)
         if data:
             self.update(data)
+        if hasattr(self, 'extra_init'):
+            self.extra_init(prefix, data=data, conf=conf)
 
     def __contains__(self, key):
         key = self.transform_key(key)
         path = os.path.join(self.conf['prefix'], key)
-        log.debug('in: %r', path)
+        log.log(LOG_TRACE, ' ? contains__(%r) <%r>', path, self)
         return (os.path.exists(path) and os.path.isfile(path))
 
     def get(self, key, default=DEFAULT):
@@ -166,46 +179,102 @@ class OnDiskKeyValueStore(KeyValueStore):
         with codecs.open(path, 'w', encoding='utf-8') as f:
             f.write(value)
 
+def shell_escape(str_):
+    """shell escape a string
+
+    Args:
+        str_ (basestring): string to 'qu\'ote and "escape"'
+    Returns:
+        unicode: str
+    """
+    return repr(unicode(str_))[1:]
+
 
 def gpg_encrypt(filelike, key_name=None):
     """
     Arguments:
-        filelike (file-like): file to read (a str will be wrapped in StringIO)
+        filelike (file-like or str): data to transform
+                                    (if str: StringIO(str))
+
+    Keyword Arguments:
+        key_name (unicode): dest_user
+
+    Returns:
+        str: subprocess.check_output(cmd, shell=True, stdin=filelike)
     """
-    cmd_tmpl = """cat | gpg --homedir ~/.gnupg --armor --encrypt -r {0}"""
-    cmd = cmd_tmpl.format(repr(key_name))
+    cmd_tmpl = ("""cat | gpg --homedir ~/.gnupg --trust-model=always"""
+                """ --encrypt --armor"""
+                """ --secret-keyring={secret_keyring} --keyring={keyring}"""
+                """ -u {user} -r {key_name}""")
+    cmd = cmd_tmpl.format(
+        user=shell_escape('p6'),
+        secret_keyring='p6.sec',
+        keyring='p6.pub',
+        key_name=shell_escape(key_name))
     if isinstance(filelike, basestring):
         filelike = StringIO.StringIO(filelike)
-    output = subprocess.check_output(cmd, stdin=filelike)
-    return output
+    log.log(LOG_TRACE, 'subprocess.Popen(%r)', cmd)
+    process = subprocess.Popen(cmd, shell=True,
+                               stdin=subprocess.PIPE,
+                               stdout=subprocess.PIPE)
+    stdout, stderr = process.communicate(input=filelike.read())
+    if (process.returncode != 0):
+        raise subprocess.CalledProcessError(
+            process.returncode, cmd, '%s\n\n## STDERR ##\n%s' % (stdout, stderr))
+    return stdout
 
 
 def gpg_decrypt(filelike, key_name=None):
     """
     Arguments:
-        filelike (file-like): file to read (a str will be wrapped in StringIO)
+        filelike (file-like or str): data to transform
+                                     (if str: StringIO(str))
+
+    Keyword Arguments:
+        key_name (unicode): dest_user
+
+    Returns:
+        str: subprocess.check_output(cmd, shell=True, stdin=filelike)
     """
-    cmd_tmpl = """cat | gpg --homedir ~/.gnupg --armor --encrypt -r {0}"""
-    cmd = cmd_tmpl.format(repr(key_name))
+    cmd_tmpl = (
+        """cat | gpg --homedir ~/.gnupg --trust-model=always"""
+        """ --decrypt"""
+        """ --secret-keyring={secret_keyring} --keyring={keyring} -u {user}""")
+    cmd = cmd_tmpl.format(
+        user=shell_escape('p6'),
+        secret_keyring='p6.sec',
+        keyring='p6.pub',
+        key_name=shell_escape(key_name))
     if isinstance(filelike, basestring):
         filelike = StringIO.StringIO(filelike)
-    output = subprocess.check_output(cmd, stdin=filelike)
-    return output
+    log.log(LOG_TRACE, 'subprocess.Popen(%r)', cmd)
+    process = subprocess.Popen(cmd, shell=True,
+                               stdin=subprocess.PIPE,
+                               stdout=subprocess.PIPE)
+    stdout, stderr = process.communicate(input=filelike.read())
+    if (process.returncode != 0):
+        raise subprocess.CalledProcessError(
+            process.returncode, cmd, '%s\n\n## STDERR ##\n%s' % (stdout, stderr))
+    return stdout
 
 
-class OnDiskGPGKeyValueStore(KeyValueStore):
+class OnDiskGPGKeyValueStore(OnDiskKeyValueStore):
+
+    def extra_init(self, prefix, data=None, conf=None, mountpt='@@@'):
+        self.mountpt = mountpt
+        self.path = os.path.join(self.conf['prefix'], self.mountpt)
+        self.disk_store = OnDiskKeyValueStore(self.path, data=data, conf=conf)
 
     def get(self, key, default=DEFAULT):
-        data = KeyValueStore.get(self, key, default=default)
+        key = self.transform_key(key)
+        data = self.disk_store.get(key, default=default)
         _value = gpg_decrypt(data, key_name=self.conf['key_name'])
         return _value
 
     def set(self, key, value):
         key = self.transform_key(key)
-        path = os.path.join(self.conf['prefix'], key)
         _value = gpg_encrypt(value, key_name=self.conf['key_name'])
-        with codecs.open(path, 'w', encoding='utf-8') as f:
-            f.write(_value)
+        return self.disk_store.set(key, _value)
 
 
 class KeyringValueStore(KeyValueStore):
@@ -228,16 +297,24 @@ class KeyringValueStore(KeyValueStore):
                 return default
             raise
 
+    def __contains__(self, key):
+        key = self.transform_key(key)
+        log.log(LOG_TRACE, ' ? contains(%r) <%r>', key, self)
+        retval = self.get(key, default=DEFAULT) != DEFAULT
+        if retval:
+            log.log(LOG_TRACE, ' X contains(%r) <%r>', key, self)
+
     def __setitem__(self, key, value):
         self.keyring.set_password(self.conf['prefix'], key, value)
 
 
 class Prompt6(KeyValueStore):
 
-    DEFAULT_PREFIX = '${__DOTFILES}/etc/keys'
+    DEFAULT_PREFIX = '-p6'  #  ${VIRTUAL_ENV}/etc/p6'
 
     def __init__(self, prefix=None, data=None, conf=None, stores=None):
         self.conf = collections.OrderedDict() if conf is None else conf
+        prefix = prefix if prefix is not None else self.DEFAULT_PREFIX
         self.set_prefix(prefix)
         self.stores = self.configure_stores(prefix=prefix,
                                             data=data,
@@ -247,25 +324,28 @@ class Prompt6(KeyValueStore):
         if stores is None:
             stores = collections.OrderedDict()
             stores['mem'] = KeyValueStore(prefix, data=data)
-            stores['disk'] = OnDiskKeyValueStore(prefix)
+            # stores['disk'] = OnDiskKeyValueStore(prefix)
+            stores['diskg'] = OnDiskGPGKeyValueStore(prefix, conf=dict(key_name='p6'))
             stores['ring'] = KeyringValueStore(prefix)
         return stores
 
     def get(self, key, default=DEFAULT, get_iter=False):
+        log.log(LOG_TRACE, 'get(%r)', key)
         output = self._get(key, default=default, get_iter=get_iter)
         if get_iter:
             return output
-        return next(iter(output))
+        if not output:
+            raise KeyError(key)
+        try:
+            return next(iter(output))
+        except StopIteration:
+            raise KeyError(key)
 
     def _get(self, key, default=DEFAULT, get_iter=False):
         value = DEFAULT
-        if key.endswith('@@'):
-            key = key[:-2]
-            store = self.stores.get('ring')
-            if store is None:
-                raise Exception('ring not configured')
-            value = store.get(key)
-            yield value
+        key, store = self.get_store_for_key(key)
+        if store:
+            yield store.get(key)
         else:
             for (name, store) in iteritems(self.stores):
                 log.debug(('%r %r' % (name, store)))
@@ -274,7 +354,7 @@ class Prompt6(KeyValueStore):
                     if get_iter:
                         yield (store, value)
                     else:
-                        if value != DEFAULT:
+                        if value != default:
                             yield value
                             return
         return
@@ -282,14 +362,41 @@ class Prompt6(KeyValueStore):
     def get_all(self, key, default=DEFAULT, get_iter=True):
         return list(self.get(key, default=default, get_iter=get_iter))
 
-    def set(self, key, value):
-        if key.endswith('@@'):
+    def get_store_for_key(self, key):
+        """
+
+        ::
+
+            <key>@@@ | <key>@@diskg -> stores['diskg']
+            <key>@@  | <key>@@ring  -> stores['ring']
+        """
+        store = None
+        if key.endswith('@@@'):
+            key = key[:-3]
+            store = self.stores.get('diskg')
+            if store is None:
+                raise Exception('diskg not configured')
+        elif key.endswith('@@diskg'):
+            key = key[:-7]
+            store = self.stores.get('diskg')
+            if store is None:
+                raise Exception('diskg not configured')
+        elif key.endswith('@@'):
             key = key[:-2]
             store = self.stores.get('ring')
             if store is None:
                 raise Exception('ring not configured')
-        else:
-            store = self.stores.get('disk')
+        elif key.endswith('@@ring'):
+            key = key[:-6]
+            store = self.store.get('ring')
+            if store is None:
+                raise Exception('ring not configured')
+        return (key, store)
+
+    def set(self, key, value):
+        key, store = self.get_store_for_key(key)
+        if store is None:
+            store = self.stores.get('diskg')
         log.debug('store: %r', store)
         store[key] = value
 
@@ -298,31 +405,99 @@ class Prompt6(KeyValueStore):
         return key
 
 
-def prompt6():
-    """
-    mainfunc
-    """
-
-
+import pprint
 import unittest
+
+
+class ConfigObj(object):
+    """Config object w/ attr access to __dict__
+
+    """
+    def __init__(self, data=None):
+        """
+        Arguments:
+            data (dict): ~self.__dict__.update(data)
+        """
+        self._reserved_keys_ = dict.fromkeys(self.__dict__.keys())
+        if data is not None:
+            self.update(data)
+
+    def update(self, dict_):
+        return self.__dict__.update(
+            {k:v for k, v in dict_.items() if k not in self._reserved_keys_})
+
+    def __str__(self):
+        return pprint.pformat(self.__dict__)
 
 
 class Test_prompt6(unittest.TestCase):
 
-    def test_prompt6(self):
-        TEST_KEY = 'test1'
-        TEST_VALUE = TEST_KEY + '#.#'
-        TEST_DATA = {TEST_KEY: TEST_VALUE}
-        p6 = Prompt6(prefix='./prefix', data=TEST_DATA)
-        self.assertTrue(p6)
-        value = p6[TEST_KEY]
-        self.assertTrue(value, TEST_VALUE)
+    def build_conf(self, **kwargs):
+        conf = ConfigObj()
+        conf.prefix = '-p6'
+        conf.TEST_KEY='test_xyz'
+        conf.TEST_VALUE=conf.TEST_KEY + '#.#'
+        conf.TEST_DATA={conf.TEST_KEY: conf.TEST_VALUE}
+        conf.update(kwargs)
+        return conf
 
+    def test_002_build_conf(self):
+        conf = self.build_conf()
+        self.assertTrue(conf)
+        self.assertEqual(conf.TEST_VALUE, conf.TEST_DATA[conf.TEST_KEY])
+
+    def test_010_prompt6(self):
+        p6 = Prompt6()
+        self.assertTrue(p6)
+        self.assertTrue(isinstance(p6, Prompt6))
+        conf = self.build_conf()
+        p6[conf.TEST_KEY] = conf.TEST_VALUE
+        value = p6[conf.TEST_KEY]
+        self.assertEqual(value, conf.TEST_VALUE)
+
+    def test_020_prompt6(self):
+        conf = self.build_conf()
+        p6 = Prompt6(prefix=conf.prefix, data=conf.TEST_DATA)
+        self.assertTrue(p6)
+        value = p6[conf.TEST_KEY]
+        self.assertTrue(value)
+        self.assertEqual(value, conf.TEST_VALUE)
+
+    def test_021(self):
+        conf = self.build_conf()
+        p6 = Prompt6(prefix=conf.prefix, data=conf.TEST_DATA)
         TEST_KEY2 = 'test2'
         TEST_VALUE2 = TEST_KEY2 + '###'
         p6[TEST_KEY2] = TEST_VALUE2
         value2 = p6[TEST_KEY2]
         self.assertEqual(value2, TEST_VALUE2)
+
+    def test_022_atat(self):
+        conf = self.build_conf()
+        p6 = Prompt6(prefix=conf.prefix, data=conf.TEST_DATA)
+        TEST_KEY2 = 'test2@@'
+        TEST_VALUE2 = TEST_KEY2 + '###@@'
+        p6[TEST_KEY2] = TEST_VALUE2
+        value2 = p6[TEST_KEY2]
+        self.assertEqual(value2, TEST_VALUE2)
+
+
+class TestPrompt6__main(unittest.TestCase):
+
+    def test_040_main(self):
+        x = main()
+        self.assertEqual(x, 0)
+
+    def test_041_main_help(self):
+        self.assertRaises(SystemExit, main, ['-h'])
+        self.assertRaises(SystemExit, main, ['--help'])
+        self.assertRaises(SystemExit, main, ['--set', '1', '2', '-h'])
+
+    def test_042_main_get(self):
+        key = '_nonexistant_key' + str(id(self))
+        args = ['--get', key]
+        ret = main(args)
+        self.assertEqual(ret, 1)
 
 
 def main(argv=None):
@@ -338,16 +513,21 @@ def main(argv=None):
 
     prs.add_option('-g', '--get',
                    dest='get',
-                   action='store')
+                   nargs=1,
+                   action='append')
     prs.add_option('-s', '--set',
                    dest='set',
                    nargs=2,
-                   action='store')
+                   action='append')
+    prs.add_option('--pw', '--set-password',
+                   dest='set_password',
+                   nargs=1,
+                   action='append')
 
     prs.add_option('-p', '--prefix',
                    dest='prefix',
                    action='store',
-                   default='./prefix')
+                   default='${VIRTUAL_ENV}/etc/p6')
 
     prs.add_option('-d', '--data',
                    nargs=2,
@@ -359,7 +539,7 @@ def main(argv=None):
 
     prs.add_option('-v', '--verbose',
                    dest='verbose',
-                   action='store_true',)
+                   action='count')
     prs.add_option('-q', '--quiet',
                    dest='quiet',
                    action='store_true',)
@@ -369,16 +549,30 @@ def main(argv=None):
     args = argv if argv is not None else []
     (opts, args) = prs.parse_args(args=args)
 
-    if not opts.quiet:
-        logging.basicConfig()
-
-        if opts.verbose:
-            logging.getLogger().setLevel(logging.DEBUG)
+    loglevel = logging.INFO
+    if opts.quiet:
+        loglevel = logging.WARNING
+    if opts.verbose == 1:
+        loglevel = logging.DEBUG
+    elif opts.verbose > 1:
+        loglevel = LOG_TRACE
+    logfmtstr = '%(asctime)s\t%(levelname)-6s\t%(message)s'
+    logging.basicConfig(
+        level=loglevel,
+        format=logfmtstr)
+    log.setLevel(loglevel)
+    log.info('opts: %r', opts)
+    # log.info('args: %r', args)
+    # if not opts.quiet:
+    #    import logging.handlers
+    #    handler = logging.handlers.SysLogHandler(
+    #        facility=logging.handlers.SysLogHandler.LOG_DAEMON)
+    #    log.addHandler(handler)
 
     if opts.run_tests:
         sys.argv = [sys.argv[0]] + args
         import unittest
-        exit(unittest.main())
+        return unittest.main()
 
     stdout = codecs.getwriter('utf-8')(sys.stdout)
 
@@ -391,14 +585,27 @@ def main(argv=None):
     p6 = Prompt6(prefix=opts.prefix, data=data)
 
     if opts.get:
-        if opts.all:
-            write(p6.get_all(opts.get))
+        try:
+            for key in opts.get:
+                if opts.all:
+                    output = p6.get_all(key)
+                else:
+                    output = p6.get(key)
+                write(output)
             return 0
-        else:
-            write(p6.get(opts.get))
-            return 0
+        except KeyError as e:
+            log.exception(e)
+            return 1
     elif opts.set:
-        p6.set(*opts.set)
+        for (key, value) in opts.set:
+            p6.set(key, value)
+        return 0
+    elif opts.set_password:
+        import getpass
+        for key in opts.set_password:
+            value = getpass.getpass("%r: " % key)
+            p6.set(key, value)
+    return 0
 
 if __name__ == "__main__":
     import sys
---
+++ scripts/this.sh
@@ -0,0 +1,173 @@
+#!/bin/bash
+
+function this_init() {
+    set -x
+    USERNAME="${1:-"p6"}"
+    HOMEDIR="${2:-"${HOME}/.gnupg"}"
+    BUILDDIR="${3:-"."}"
+    PUBRING="${USERNAME}.pub"
+    SECRING="${USERNAME}.sec"
+    EMAIL="${USERNAME}@localhost"
+    KEYTYPE="default"
+    SUBKEYTYPE="default"
+    NAME_REAL="${USERNAME}"
+    NAME_COMMENT="\#${USERNAME}"
+    EXPIRE_DATE="0"
+}
+
+function this_generate_key() {
+    set +x
+    PASSPHRASE="${1}"  # "abc"
+    PASSPHRASESTR=""
+    if [ -n "${PASSPHRASE}" ]; then
+        PASSPHRASESTR="Passphrase: ${PASSPHRASE}"
+    fi
+    set -x
+    echo - | gpg2 --gen-key --batch << EOF
+%echo Generating a default key
+Key-Type: ${KEYTYPE}
+Subkey-Type: ${SUBKEYTYPE}
+Name-Real: ${NAME_REAL}
+Name-Comment: ${NAME_COMMENT}
+Name-Email: ${EMAIL}
+Expire-Date: ${EXPIRE_DATE}
+${PASSPHRASESTR}
+%pubring ${BUILDDIR}/${PUBRING}
+%secring ${BUILDDIR}/${SECRING}
+# Do a commit here, so that we can later print "done" :-)
+%commit
+%echo done
+EOF
+    this_list_keys
+}
+
+function this_list_keys() {
+    local builddir=${1:-${BUILDDIR}}
+    gpg2 \
+        --no-default-keyring \
+        --secret-keyring="${builddir}/${SECRING}" \
+        --keyring="${builddir}/${PUBRING}" \
+        --list-secret-keys \
+        --fingerprint
+}
+
+
+function copy_backup() {
+    set -x
+    local src=${1}
+    local dest=${2}
+    local datetime=${3:-$(date +'%FT%T%z')}
+    if [ -f "${dst}" ]; then
+        rsync -pv "${dst}" "${dst}.bkp.${datetime}"
+    fi
+    rsync -pv "${src}" "${dst}"
+}
+
+function push_new_keyset() {
+    set -x
+    local builddir=${1:-${BUILDDIR}}
+    local destdir=${2:-${HOMEDIR}}
+    local SECRING=${SECRING}
+    local PUBRING=${PUBRING}
+    SECRING_SRC=${builddir}/${SECRING}
+    PUBRING_SRC=${builddir}/${PUBRING}
+    SECRING_DST=${destdir}/${SECRING}
+    PUBRING_DST=${destdir}/${PUBRING}
+    copy_backup "${SECRING_SRC}" "${SECRING_DST}"
+    copy_backup "${PUBRING_SRC}" "${PUBRING_DST}"
+}
+
+function this_encode() {
+    ## XXX: --trust-model always (allow use unsigned key (${EMAIL}))
+    gpg2 \
+        --no-default-keyring \
+        --secret-keyring="${BUILDDIR}/${SECRING}" \
+        --keyring="${BUILDDIR}/${PUBRING}" \
+        --trust-model=always \
+        --armor \
+        --encrypt \
+        -r ${EMAIL}
+}
+
+function this_decode() {
+    gpg2 \
+        --no-default-keyring \
+        --secret-keyring="${BUILDDIR}/${SECRING}" \
+        --keyring="${BUILDDIR}/${PUBRING}" \
+        --decrypt
+}
+
+function this_test() {
+    TEST_STRING='test_one_two_two\n#123\n\n.';
+    output="$(echo "${TEST_STRING}" | this_encode | this_decode)"
+    if [[ "${output}" != "${TEST_STRING}" ]]; then
+        echo "FAIL: assertEqual(output, TEST_STRING)"
+        echo "# ${output}"
+        echo "# ${TEST_STRING}"
+        return -1
+    else
+        echo "PASS: assertEqual(output, TEST_STRING)"
+    fi
+}
+
+function this__() {
+    # for prompt6,
+    # i think it makes sense to 
+    # encode for a stable p6 key        # -r p6@localhost
+    # encode with (one of) other keys   # -u user@domain 
+    #
+    # this way, one or more keys can be used to update values
+    # without having to re-encode the whole tree
+    echo ''
+}
+
+function this_help () {
+    echo "${0} -- keygen/encode/decode wrapper"
+    echo ""
+    echo "  -G  -- Generate new key"
+    echo "  -l  -- List keys and fingerprints"
+    echo "  -t  -- Run Tests"
+    echo "  -h  -- Print help"
+    echo ""
+
+}
+
+function this_main () {
+    while getopts "Glth" opt; do
+        case "${opt}" in
+            G)
+                _this_generate_key=true;
+                ;;
+            l)
+                _this_list_keys=true;
+                ;;
+            t)
+                _this_run_tests=true;
+                ;;
+            *|h)
+                _this_help=true;
+                ;;
+        esac
+    done
+    test -n ${_this_help} && this_help && return 0
+
+    this_init
+
+    if [ -n ${_this_list_keys} ]; then
+        this_list_keys
+    fi
+    if [ -n ${_this_generate_key} ]; then
+        this_generate_key
+        if [ -n ${_this_list_keys} ]; then
+            this_list_keys
+        fi
+    fi
+    if [ -n ${_this_run_tests} ]; then
+        this_test
+    fi
+    return $?
+}
+if [[ "${BASH_SOURCE}" == "${0}" ]]; then
+    this_main ${@}
+    exit
+fi
