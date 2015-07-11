acad63cb44fdbeb326ba6a26a216bd594f3785b0
Wes Turner <wes@wrd.nu>
2015-07-10 19:39:40 -0500

    ENH: pyline.py, pyline: vendor latest release pyline, ln -s ./pyline.py ./pyline

--- scripts/pyline.py
+++ scripts/pyline.py
@@ -538,7 +538,7 @@ class ResultWriter_csv(ResultWriter):
 class ResultWriter_json(ResultWriter):
     filetype = 'json'
 
-    def write_numbered(self, obj):
+    def write(self, obj):
         print(
             json.dumps(
                 obj._asdict(),
@@ -546,13 +546,7 @@ class ResultWriter_json(ResultWriter):
             end=',\n',
             file=self._output)
 
-    def write(self, obj):
-        print(
-            json.dumps(
-                obj.result,
-                indent=2),
-            end=',\n',
-            file=self._output)
+    write_numbered = write
 
 
 class ResultWriter_html(ResultWriter):
