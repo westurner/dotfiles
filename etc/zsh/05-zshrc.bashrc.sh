b5d3e9a6f4129bb65077454faca85a6bd6adb5fc
Wes Turner <wes@wrd.nu>
2015-07-25 05:15:34 -0500

    BUG: scripts/usrlog.sh: zsh _usrlog_set_HISTFILE (TODO compat)

--- etc/zsh/05-zshrc.bashrc.sh
+++ etc/zsh/05-zshrc.bashrc.sh
@@ -3,4 +3,5 @@
 #  bash_source function
 #  $__DOTFILES
 
-bash_source $__DOTFILES/etc/bash/00-bashrc.before.sh
+export __IS_ZSH="${__IS_ZSH:-"1"}"
+bash_source "${__DOTFILES}/etc/bash/00-bashrc.before.sh"
