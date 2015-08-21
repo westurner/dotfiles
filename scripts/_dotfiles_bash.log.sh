295f088a1ab0d247c479de8729f176b38311f942
Wes Turner <wes@wrd.nu>
2015-08-20 22:58:13 -0500

    DOC: autogenerated usage docs

--- scripts/_dotfiles_bash.log.sh
+++ scripts/_dotfiles_bash.log.sh
@@ -1190,25 +1190,25 @@ workon_conda_if_available() {
 #               _SRC=${VIRTUAL_ENV}/${_APP}
 #               _WRD=${VIRTUAL_ENV}/${_APP}
 
-_setup_virtualenvwrapper_default_config() {
+function _setup_virtualenvwrapper_default_config {
     export __WRK="${__WRK:-"${HOME}/workspace"}"
     export PROJECT_HOME="${__WRK}"
     export WORKON_HOME="${HOME}/.virtualenvs"
 }
-_setup_virtualenvwrapper_dotfiles_config() {
+function _setup_virtualenvwrapper_dotfiles_config {
     export __WRK="${__WRK:-"${HOME}/-wrk"}"
     export PROJECT_HOME="${__WRK}"
     export WORKON_HOME="${__WRK}/-ve27"
 }
 
-_setup_virtualenvwrapper_dirs() {
+function _setup_virtualenvwrapper_dirs {
     umask 027
     mkdir -p "${__WRK}" || chmod o-rwx "${__WRK}"
     mkdir -p "${PROJECT_HOME}" || chmod o-rwx "${PROJECT_HOME}"
     mkdir -p "${WORKON_HOME}" || chmod o-rwx "${WORKON_HOME}"
 }
 
-_setup_virtualenvwrapper_config () {
+function _setup_virtualenvwrapper_config  {
     # _setup_virtualenvwrapper_config()    -- configure $VIRTUALENVWRAPPER_*
     #export VIRTUALENVWRAPPER_SCRIPT="/usr/local/bin/virtualenvwrapper.sh"
     #export VIRTUALENVWRAPPER_SCRIPT="${HOME}/.local/bin/virtualenvwrapper.sh"
@@ -1243,7 +1243,7 @@ _setup_virtualenvwrapper_config () {
 
 }
 
-lsvirtualenvs() {
+function lsvirtualenvs {
     # lsvirtualenvs()       -- list virtualenvs in $WORKON_HOME
     cmd=${@:-""}
     (cd ${WORKON_HOME} &&
@@ -1256,12 +1256,12 @@ lsvirtualenvs() {
         fi
     done)
 }
-lsve() {
+function lsve {
     # lsve()                -- list virtualenvs in $WORKON_HOME
     lsvirtualenvs $@
 }
 
-backup_virtualenv() {
+function backup_virtualenv {
     # backup_virtualenv()   -- backup VIRTUAL_ENV_NAME $1 to [$2]
     venv=${1}
     _date=$(date +'%FT%T%z')
@@ -1275,7 +1275,7 @@ backup_virtualenv() {
         || (echo "err: ${venv} (${archivename})" 1>&2))
 }
 
-backup_virtualenvs() {
+function backup_virtualenvs {
     # backup_virtualenvs()  -- backup all virtualenvs in $WORKON_HOME to [$1]
     date=$(date +'%FT%T%z')
     bkpdir=${1:-"${WORKON_HOME}/_venvbkps/${date}"}
@@ -1293,7 +1293,7 @@ backup_virtualenvs() {
     echo BKPDIR="${bkpdir}"
 }
 
-_rebuild_virtualenv() {
+function _rebuild_virtualenv {
     # rebuild_virtualenv()      -- rebuild a virtualenv, leaving pkgs in place
     #    $1="$VENVSTR"
     #    $2="$VIRTUAL_ENV"
@@ -1340,14 +1340,14 @@ _rebuild_virtualenv() {
     "
 }
 
-rebuild_virtualenv() {
+function rebuild_virtualenv {
     #  rebuild_virtualenv()     -- rebuild a virtualenv
     #    $1="$VENVSTR"
     #    $2="$VIRTUAL_ENV"
     (set -x; _rebuild_virtualenv $@)
 }
 
-rebuild_virtualenvs() {
+function rebuild_virtualenvs {
     # rebuild_virtualenvs()     -- rebuild all virtualenvs in $WORKON_HOME
     lsve rebuild_virtualenv
 }
@@ -1355,7 +1355,7 @@ rebuild_virtualenvs() {
 
 _setup_virtualenvwrapper_dotfiles_config  # ~/-wrk/-ve27 {-ve34,-ce27,-ce34}
 
-_setup_virtualenvwrapper() {
+function _setup_virtualenvwrapper {
   # _setup_virtualenvwrapper_default_config # ~/.virtualenvs/
   _setup_virtualenvwrapper_config
   _setup_virtualenvwrapper_dirs
