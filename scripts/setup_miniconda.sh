54bf7bace40ffc0669582ea735b020b8711428aa
Wes Turner <wes@wrd.nu>
2015-08-22 18:13:14 -0500

    ENH: scripts/setup_miniconda.sh: varnames, default setup

--- scripts/setup_miniconda.sh
+++ scripts/setup_miniconda.sh
@@ -36,7 +36,8 @@ function miniconda_download {
     #    $1 (str)  -- versionstr to append (3 or "")
     #    $2 (str)  -- destination directory in which to store downloaded files
     #    Returns:
-    #       
+    #      1:  -- download err
+    #      0:  -- download OK
     local verstr="${1}"
     local destdir="${2:-"${DOWNLOAD_DEST}"}"
     local filename=$(miniconda_get_download_filename "${verstr}")
@@ -49,32 +50,64 @@ function miniconda_download {
         && return 0
     return 1
 }
+
 function miniconda_install {
+    #  miniconda_install()   -- run the miniconda installer for the
+    #    $1 (str)  -- path to miniconda installer .sh
+    #    $2 (str)  -- install dest (default: ~/miniconda; __WRK/-conda[27|34])
+    #    Returns:
+    #      n: Miniconda[3]-latest-*.sh return code
     local miniconda_installer_sh="${1}"
     local prefix="${2:-"${HOME}/miniconda"}"
     shift
     shift
     sh ${miniconda_installer_sh} -b -p "${prefix}" ${@}
 }
-function miniconda_setup__dotfiles_defaults {
+
+function miniconda_setup__dotfiles_minicondas {
+    #  miniconda_setup__dotfiles_minicondas  -- install CONDA_ROOTs
+    #    $1 (str)  -- path for CONDA_ROOTs and CONDA_HOMEs (default: $__WRK)
+    #    Returns:
+    #      n: sum of Miniconda[3]-latest-*.sh return codes
     local prefix="${1:-"${__WRK}"}"
-    local conda_root__py27="${CONDA_ROOT__py27:-"${prefix}/-conda27"}"
-    local conda_root__py34="${CONDA_ROOT__py34:-"${prefix}/-conda34"}"
+    local CONDA_ROOT__py27="${CONDA_ROOT__py27:-"${prefix}/-conda27"}"
+    local CONDA_ROOT__py34="${CONDA_ROOT__py34:-"${prefix}/-conda34"}"
 
     local mc2=$(miniconda_download)
     echo $mc2
     local mc3=$(miniconda_download 3)
     echo $mc3
 
-    miniconda_install "${mc2}" "${conda_root__py27}"
+    local ret=0;
+    miniconda_install "${mc2}" "${CONDA_ROOT__py27}"
+    ret=$(expr $ret+$?);
     #miniconda_install "${mc2}" "${__WRK}/-ace34"
-    miniconda_install "${mc3}" "${conda_root__py34}"
-
-    ##  Then, to create environments with each conda instance:
-    #${CONDA_ROOT__py27}/bin/conda env create -n name python   readline pip
-    #${CONDA_ROOT__py34}/bin/conda env create -n name python=3 readline pip
+    miniconda_install "${mc3}" "${CONDA_ROOT__py34}"
+    ret=$(expr $ret+$?);
+    return $ret;
 }
+
+function miniconda_setup__dotfiles_condaenvs {
+    local prefix="${1:-"${__WRK}"}"
+    local CONDA_ROOT__py27="${CONDA_ROOT__py27:-"${prefix}/-conda27"}"
+    local CONDA_ROOT__py34="${CONDA_ROOT__py34:-"${prefix}/-conda34"}"
+    local CONDA_HOME__py27="${CONDA_HOME__py27:-"${prefix}/-ce27"}"
+    local CONDA_HOME__py34="${CONDA_HOME__py34:-"${prefix}/-ce34"}"
+
+    ## create environments with each conda instance:
+    "${CONDA_ROOT__py27}/bin/conda" install -y -n root conda-env python pip readline
+    "${CONDA_ROOT__py34}/bin/conda" install -y -n root conda-env python=3 pip readline
+    test -d "${CONDA_HOME__py27}/dotfiles" || "${CONDA_ROOT__py27}/bin/conda" create -n dotfiles
+    test -d "${CONDA_HOME__py34}/dotfiles" || "${CONDA_ROOT__py34}/bin/conda" create -n dotfiles
+}
+
+function miniconda_setup_main {
+    ## install miniconda and configure condaenvs
+    miniconda_setup__dotfiles_minicondas ${@};
+    miniconda_setup__dotfiles_condaenvs ${@};
+}
+
 if [ "${BASH_SOURCE}" == "${0}" ]; then
-    echo ${@}
-    miniconda_setup__dotfiles_defaults ${@}
+    miniconda_setup_main "${@}"
+    exit
 fi
