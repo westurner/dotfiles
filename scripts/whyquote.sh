d9c380d6e4274c48f6dd88dced645e425ca041e3
Wes Turner <wes@wrd.nu>
2015-10-05 21:21:27 -0500

    DOC,SEC: whyquote.sh: why shell quoting matters

--- scripts/whyquote.sh
+++ scripts/whyquote.sh
@@ -3,39 +3,50 @@
 ### whyquote.sh
 # why shell quoting matters
 
+function example {
+    echo ""
+    echo "###: $@"
+}
+
 a="; echo '!$!'"
 
-
+example 1
 echo $a
 echo ${a}
 echo "$a"
 echo "${a}"
 
 
+example one
 echo "one" $a
 echo "one" ${a}
 echo "one" "$a"
 echo "one" "${a}"
 
 # ..
-
+example echo_one
 cmd="echo 'one'; ${a}"
 echo ${cmd}
 echo "${cmd}"
 ${cmd}
 "${cmd}"
 
+example 10
+example10=$(pwd)
+echo $example10
+echo "$example10"
 
-example2=$(pwd)
-echo $example2
-echo "$example2"
+example 11
+example11="$(pwd)"
+echo $example11
+echo "$example11"
 
+example 20
+example20="-e \nthis"
+echo $example20
+echo "$example20"
 
-example3="$(pwd)"
-echo $example3
-echo "$example3"
-
-
-example3="-e \nthis"
-echo $example3
-echo "$example3"
+example 21
+example21="-e \n#this"
+echo $example21
+echo "$example21"
