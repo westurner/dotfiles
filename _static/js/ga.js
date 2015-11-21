174aab70f54bd9b6e644cb910a8d1f5af14a95a0
Wes Turner <wes@wrd.nu>
2015-11-21 02:51:15 -0600

    Update documentation

--- _static/js/ga.js
+++ _static/js/ga.js
@@ -4,8 +4,7 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
 var keymap = {
-    "www.wrdrd.com": 'UA-55346955-1',
-    "wrdrd.com": 'UA-55346955-2'
+    "westurner.org": 'UA-46001060-1',
 };
 
 if (document.location.hostname in keymap) {
