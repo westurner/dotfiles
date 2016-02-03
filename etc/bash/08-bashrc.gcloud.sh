2d626e2b27c98a7742843e840478e4a338ace227
Wes Turner <wes@wrd.nu>
2016-02-02 20:05:00 -0600

    REF,BUG: 08-bashrc.gcloud.sh: _GCLOUD_PREFIX -> GCLOUD_BASEPATH

--- etc/bash/08-bashrc.gcloud.sh
+++ etc/bash/08-bashrc.gcloud.sh
@@ -3,11 +3,12 @@
 
 _setup_google_cloud() {
     # _setup_google_cloud() -- configure gcloud $PATH and bash completions
-    export _GCLOUD_PREFIX="/srv/wrk/google-cloud-sdk"
+    export GCLOUD_BASEPATH="${HOME}/google-cloud-sdk"
+    #export GCLOUD_BASEPATH="/srv/wrk/google-cloud-sdk"
 
     #The next line updates PATH for the Google Cloud SDK.
-    source "${_GCLOUD_PREFIX}/path.bash.inc"
+    source "${GCLOUD_BASEPATH}/path.bash.inc"
 
     #The next line enables bash completion for gcloud.
-    source "${_GCLOUD_PREFIX}/completion.bash.inc"
+    source "${GCLOUD_BASEPATH}/completion.bash.inc"
 }
