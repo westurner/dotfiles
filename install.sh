a60afe1af84c2052739ac6c9c185ff01661c00aa
Wes Turner <wes@wrd.nu>
2016-02-04 17:59:41 -0600

    BLD,DOC: install.sh: wrap(bootstrap_dotfiles.sh)

--- install.sh
+++ install.sh
@@ -3,12 +3,21 @@
 # Create a directory, clone, install, and source the dotfiles
 # https://github.com/westurner/dotfiles
 
-_SRC="./-wrk/-ve27/dotfiles/src"; mkdir -p "${_SRC}"
-_WRD="${_SRC}/dotfiles"
-git clone https://github.com/westurner/dotfiles -b develop "${_WRD}"
-bash "${_WRD}"/scripts/bootstrap_dotfiles -S
-bash "${_WRD}"/scripts/bootstrap_dotfiles -C
-#bash "${_WRD}"/scripts/bootstrap_dotfiles -I -U
+
+WORKON_HOME="${WORKON_HOME:-"${HOME}/-wrk/-ve27"}" # ~/-wrk/-ve27
+VIRTUAL_ENV="${WORKON_HOME}/dotfiles"              # ~/-wrk/-ve27/dotfiles
+_SRC="${VIRTUAL_ENV}/src"; mkdir -p "${_SRC}"      # ~/-wrk/-ve27/dotfiles/src
+_WRD="${_SRC}/dotfiles"                   # ~/-wrk/-ve27/dotfiles/src/dotfiles
+
+GITURL='https://github.com/westurner/dotfiles'
+GITBRANCH="${GITBRANCH:-'develop'}"
+
+git clone --depth=3 "${GITURL}" -b "${GITBRANCH}" "${_WRD}"
+bash "${_WRD}"/scripts/bootstrap_dotfiles.sh -S -C # create symlinks, check
+
+test -n "${INSTALL_INTO_USER}" && \
+    bash "${_WRD}"/scripts/bootstrap_dotfiles.sh -I -R -u  # install w/ --user
+
 ls -ald ~/-dotfiles
 ls -ald ~/.bashrc
 source ~/.bashrc
