81c9dda9d48731459b46c45ea72d541014a70f7b
Wes Turner <wes@wrd.nu>
2016-02-04 17:20:22 -0600

    ENH,REF,DOC: scripts/new.sh: updated template

--- scripts/new.sh
+++ scripts/new.sh
@@ -1,32 +1,54 @@
 #!/bin/sh
-## new.sh
+### new.sh -- generate a templated shell script
+# see also: etc/vim/snippets-ulti/sh.snippets
+#   vim: :w file.sh
+#   vim "bashscript"<ctrl><enter>  -- expand the template
+#   # <ctrl> j -- next tmplfield
+#   # <ctrl> k -- prev tmplfield
 
-function _new_sh () {
-    # new_sh()          -- create and open a new shell script at $1
-    file=$1
-    if [ -e $1 ]; then
-        echo "$1 exists"
-    else
-        touch $1
-        echo "#!/bin/sh" >> ${1}
-        echo "## " >> ${1}
-        echo "" >> ${1}
-        echo 'if [ "${BASH_SOURCE}" == "${0}" ]; then' >> ${1}
-        echo '    echo ${@}' >> ${1}
-        echo 'fi' >> ${1}
-        chmod 700 ${1}
-        ${EDITOR_} +3 ${1}
-    fi
+
+function new_sh {
+    ## new_sh()          -- create and open a new shell script at $1
+    #  $1 (path)   -- path
+    function _new_sh {
+        local file="${1}"
+        if [ -e "${1}" ]; then
+            echo "${1} exists"
+            return 1
+        fi
+        touch "${1}"
+        echo "#!/usr/bin/env bash" >> "${1}"
+        echo "### ${1} ## description_commentline" >> "${1}"
+        echo '' >> "${1}"
+        echo 'function main {' >> "${1}"
+        echo '    ## main()     ## call main' >> "${1}"
+        echo '    echo "${@}"' >> "${1}"
+        echo '    ' >> "${1}"
+        echo '}' >> "${1}"
+        echo '' >> "${1}"
+        echo 'if [ -n "${BASH_SOURCE}" ] && [ "${BASH_SOURCE}" == "${0}" ]; then' >> "${1}"
+        echo '    echo ${@}' >> "${1}"
+        echo 'fi' >> "${1}"
+        chmod 700 "${1}"
+
+        function _edit {
+            local edit="$(which edit 2>/dev/null)"
+            if [ -n "${edit}" ]; then
+                edit ${@}
+            else
+                ${EDITOR_:-${EDITOR}} ${@}
+            fi
+            return
+        }
+        _edit +7 "${file}"
+        return
+    }
+    (set -x; _new_sh ${@})
     return
 }
 
-function new_sh() {
-    (set -x; _new_sh ${@}; return)
-    return
-}
 
-
-if [ "${BASH_SOURCE}" == "${0}" ]; then
+if [ -n "${BASH_SOURCE}" ] && [ "${BASH_SOURCE}" == "${0}" ]; then
     new_sh ${@}
     exit
 fi
