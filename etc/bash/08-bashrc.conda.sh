20890b98c2994c956cc962525d0d366063364eab
Wes Turner <wes@wrd.nu>
2016-04-11 04:56:38 -0500

    BUG,DOC,ENH,UBY: 08-bashrc.conda.sh: -h/--help, lscondaenvs opts, py35 defaults; WIP

--- etc/bash/08-bashrc.conda.sh
+++ etc/bash/08-bashrc.conda.sh
@@ -554,7 +554,7 @@ function mkvirtualenv_conda {
 
     # TODO: set VIRTUAL_ENV and call dotfiles_postmkvirtualenv
     # export VIRTUAL_ENV="${CONDA_ENV}"
-    workon_conda "${_conda_envname}" "${_conda_envs_path}" "${CONDA_ROOT_PATH}"
+    workon_conda "${_conda_envname}" "${_conda_envs_path}" "${CONDA_ROOT}"
     export VIRTUAL_ENV="${CONDA_ENV}"
 
     # if there is a function named 'dotfiles_postmkvirtualenv',
