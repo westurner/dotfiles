d0dfac962ea30409ec05e3e43873b4f3d5b98b18
Wes Turner <wes@wrd.nu>
2019-04-14 14:22:41 -0400

    DOC,BLD: conf.py: include sphinxjp.themes.basicstrap as an extension

--- docs/conf.py
+++ docs/conf.py
@@ -65,11 +65,13 @@ import dotfiles
 # Add any Sphinx extension module names here, as strings.
 # They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
+import sphinxjp.themes.basicstrap
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinxcontrib.napoleon',
-    'sphinxcontrib.srclinks'
+    'sphinxcontrib.srclinks',
+    'sphinxjp.themes.basicstrap',
 ]
 
 try:
