b329707b85f2fcc2e3509f19c307c571c2c5c09f
Wes Turner <wes@wrd.nu>
2019-12-26 01:56:45 -0500

    CLN: 50-bashrc.bashmarks.sh: shebang, tighter regex, shellcheck

--- etc/bash/50-bashrc.bashmarks.sh
+++ etc/bash/50-bashrc.bashmarks.sh
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env bash
 ### bashrc.bashmarks.sh
 ## bashmarks
     # l()  -- list bashmarks
@@ -6,10 +6,11 @@
     # g()  -- goto bashmark $1
     # p()  -- print bashmark $1
     # d()  -- delete bashmark $1
+# shellcheck source=../bashmarks/bashmarks.sh
 source "${__DOTFILES}/etc/bashmarks/bashmarks.sh"
 lsbashmarks () {
     # lsbashmarks() -- list Bashmarks (e.g. for NERDTree)
-    export | grep 'DIR_' | pyline "line[15:].replace('\"','').split('=',1)"
+    export -p | grep -E 'DIR_\w+="' | pyline "line[15:].replace('\"','').split('=',1)"
 }
 
     # see also: ${__DOTFILES}/scripts/nerdtree_to_bashmarks.py
