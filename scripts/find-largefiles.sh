a3c5f2aa2492a0981a94b5c23f57bb4fb479cd87
Wes Turner <wes@wrd.nu>
2020-02-09 10:03:51 +0000

    ENH,BUG,SEC: find-largefiles.sh: quoting, pass ${1+} through to ls command

--- scripts/find-largefiles.sh
+++ scripts/find-largefiles.sh
@@ -2,18 +2,21 @@
 ## find-largefiles.sh
 
 function find_largefiles () {
-    (set -x; _find_largefiles ${@}; return)
+    (set -x; _find_largefiles "${@}"; return)
     return
 }
 
 function _find_largefiles () {
     # find-largefiles() -- find files larger than size (default: +10M)
     SIZE=${1:-"+10M"}
-    find . -xdev -type f -size "${SIZE}" -exec ls -al {} \;
+    if [ -n "${1}" ]; then
+        shift
+    fi
+    find . -xdev -type f -size "${SIZE}" -exec ls -al "${@}" {} \;
     return
 }
 
 if [ -n "${BASH_SOURCE}" ] && [ "${BASH_SOURCE}" == "${0}" ]; then
-    find_largefiles ${@}
+    find_largefiles "${@}"
     exit
 fi
