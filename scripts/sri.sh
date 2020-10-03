beb3bc9a46cab4793d758a29fbf71a8169415915
Wes Turner <wes@wrd.nu>
2020-10-02 21:11:35 -0400

    ENH: sri.sh: add --js and --css args to print the full <script> or <link> line

--- scripts/sri.sh
+++ scripts/sri.sh
@@ -14,9 +14,25 @@ print_usage() {
     echo 'Usage:'
     echo '  $ sha384sum -b ./bootstrap.min.css | sri.sh'
     echo ''
+    echo '  $ sri.sh --css bootstrap.min.js'
+    echo '  $ SRIURLPATH="./static/js" sri.sh --css bootstrap.min.js'
+    echo ''
+    echo ''
     echo 'Subresource Integrity (SRI) References:'
     echo '- https://www.w3.org/TR/SRI/'
     echo '- https://developer.mozilla.org/en-US/docs/Web/Security/Subresource_Integrity'
+}
+
+print_css_tag() {
+    f=$1
+    path=${SRIURLPATH}
+    echo "<link rel=\"stylesheet\" src=\"${SRIURLPATH}/${f}\" integrity=\"sha384-$(sha384sum "${f}" | sri_encode )\" crossorigin=\"anonymous\">"
+}
+
+print_js_tag() {
+    f=$1
+    path=${SRIURLPATH}
+    echo "<script src=\"${SRIURLPATH}/${f}\" integrity=\"sha384-$(sha384sum "${f}" | sri_encode )\" crossorigin=\"anonymous\"></script>"
 }
 
 
@@ -26,6 +42,19 @@ main() {
         -h|--help)
           print_usage;
           exit
+          ;;
+        --css)
+          shift;
+          print_css_tag "${1}"
+          shift
+          exit
+          ;;
+        --js)
+          shift;
+          print_js_tag "${1}"
+          shift
+          exit
+          ;;
       esac
     done
     sri_encode
