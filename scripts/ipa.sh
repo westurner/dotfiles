802b0386b2d01bd9a6d149e3ac2cab7fc16bbf8f
Wes Turner <wes@wrd.nu>
2020-11-24 18:31:33 -0500

    ENH: scripts/ipa.sh: ~grep `ip a` with pyline

---
+++ scripts/ipa.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+ip a | pyline -r '^((\d+):\s+(.*))|^\s+(inet6? (.*))' '[obj for obj in [(row[2] if row[2] else "\n"+row[1]) if row else None for row in [rgx.groups()[1:] if rgx else None]] if obj]'
