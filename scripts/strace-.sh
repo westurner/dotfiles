216566aba935ae672cce1af2fc5567cf9f9eb506
Wes Turner <wes@wrd.nu>
2025-09-14 14:09:16 -0400

    BUG,SEC: strace-.sh, strace-f-noeno.sh: quote "${@}"

--- scripts/strace-.sh
+++ scripts/strace-.sh
@@ -2,16 +2,16 @@
 ## strace-.sh
 
 function _strace_ () {
-    strace -ttt -f -F $@ 2>&1
+    strace -ttt -f -F "$@" 2>&1
     return
 }
 
 function strace_ () {
-    (set -x; _strace_ ${@}; return)
+    (set -x; _strace_ "${@}")
     return
 }
 
 if [ -n "${BASH_SOURCE}" ] && [ "${BASH_SOURCE}" == "${0}" ]; then
-    strace_ ${@}
+    strace_ "${@}"
     exit
 fi
--- scripts/strace-f-noeno.sh
+++ scripts/strace-f-noeno.sh
@@ -1,23 +1,25 @@
 #!/bin/sh
 ## strace-f-noeno.sh
 
+set -x
+
 #HERE="${__DOTFILES}/scripts"
 HERE=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source ${HERE}/strace-.sh
 
 function strace_f_noeno () {
-    (set -x; _strace_f_noeno ${@}; return)
+    (set -x; _strace_f_noeno "${@}"; return)
     return
 }
 
 function _strace_f_noeno () {
     # strace-f-noeno()  -- strace -e trace=file | grep -v ENOENT
-    _strace_ -e trace=file $@ 2>&1 \
-        | grep -v '-1 ENOENT (No such file or directory)$' 
+    _strace_ -e trace=file "$@" 2>&1 \
+        | grep -v '\-1 ENOENT (No such file or directory)$'
     return
 }
 
 if [ -n "${BASH_SOURCE}" ] && [ "${BASH_SOURCE}" == "${0}" ]; then
-    strace_f_noeno ${@}
+    strace_f_noeno "${@}"
     exit
 fi
