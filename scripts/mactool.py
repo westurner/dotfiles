f53b3b6ec873c9939a03c877750739763027bf21
Wes Turner <wes@wrd.nu>
2026-01-20 03:16:14 -0500

    CLN,REF: mactool.py: syntax, argparse

--- scripts/mactool.py
+++ scripts/mactool.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
-from os import getenv
-from random import randrange,seed
+
+import re
 import sys
+from os import getenv
+from random import randrange, seed
 
 if sys.version_info.major > 2:
     from urllib.request import urlopen
@@ -12,96 +14,115 @@ _OUI_URL = "https://standards-oui.ieee.org/oui/oui.txt"
 _MACFILE = "%s/.macvendors" % getenv("HOME")
 
 
-_V_VMWARE = '00:0c:29'
+_V_VMWARE = "00:0c:29"
 
-def rand_hex(num):
-    return [hex(randrange(0,255)).split("x")[1].zfill(2) for n in range(0,num)]
+
+def rand_hex(num:int) -> list[str]:
+    return [hex(randrange(0, 255)).split("x")[1].zfill(2) for n in range(0, num)]
+
 
 def rand_vmware():
-    l = _V_VMWARE.split(":")
-    l.extend(rand_hex(3))
-    return ':'.join(l)
+    line = _V_VMWARE.split(":")
+    line.extend(rand_hex(3))
+    return ":".join(line)
 
-rand_global = lambda: ':'.join(rand_hex(6))
+
+def rand_global():
+    return ":".join(rand_hex(6))
+
 
 def format_line(line):
     s = line.split()
-    return (s[0], ' '.join(x.lower().capitalize() for x in s[2:]))
+    return (s[0], " ".join(x.lower().capitalize() for x in s[2:]))
+
 
 def download_oui():
-    #f = open(filename,"r")
+    # f = open(filename,"r")
     f = urlopen(_OUI_URL)
-    with open(_MACFILE,"w+") as o:
-        lines = (format_line(l) for l in f.readlines() if "(hex)" in l)
-        o.writelines( ','.join(x)+"\n" for x in lines)
+    with open(_MACFILE, "w+") as o:
+        lines = (format_line(line) for line in f.readlines() if "(hex)" in line)
+        o.writelines(",".join(x) + "\n" for x in lines)
         o.close()
     print("OUI File downloaded to %s" % _MACFILE)
 
+
 def mac_to_vendor(mac):
-    f = open(_MACFILE,"r")
-    mac = mac.replace(":","-")[:8].upper()
+    with open(_MACFILE, "r") as f:
+        mac = mac.replace(":", "-")[:8].upper()
 
-    return '\n'.join(
-        ''.join(x.split(",")[1:]).strip() for x in f if x.startswith(mac))
+    return "\n".join("".join(x.split(",")[1:]).strip() for x in f if x.startswith(mac))
 
-def find_vendor(vendor):
-    f = open(_MACFILE,"r")
+
+def find_vendor(vendor:str):
     vendor = vendor.lower()
-    return '\n'.join(
-        x.replace('-',':').strip() for x in f if vendor in f.lower())
+    with open(_MACFILE, "r") as f:
+        return "\n".join(x.replace("-", ":").strip() for x in f.readlines() if re.match(vendor, str(f), re.IGNORECASE))
+
 
 def mac_from_prefix(prefix):
-    p = prefix.replace("-",":")[:8]
-    l = p.split(":")
-    if len(l) != 3:
+    prefixstr = prefix.replace("-", ":")[:8]
+    line = prefixstr.split(":")
+    if len(line) != 3:
         raise Exception("Bad prefix")
-    l.extend(rand_hex(3))
-    return (':'.join(l)).upper()
+    line.extend(rand_hex(3))
+    return (":".join(line)).upper()
+
 
 def mac_from_vendor(vendor):
-    l = [x.split(",")[0] for x in find_vendor(vendor).split("\n")]
-    return mac_from_prefix(l[randrange(0,len(l))])
+    line = [x.split(",")[0] for x in find_vendor(vendor).split("\n")]
+    return mac_from_prefix(line[randrange(0, len(line))])
 
-if __name__=="__main__":
-    _VERSION = 0.1
-    seed()
 
-    from optparse import OptionParser
-    arg = OptionParser(version="%prog 0.1")
-    arg.add_option("-d","--download",
-            dest="download",
-            action="store_true",
-            help="Download latest MAC list")
-    arg.add_option("-m","--mac",
-            dest="mac",
-            help="MAC Address to Convert to Vendor String")
-    arg.add_option("-v","--vendor",
-            dest="vendor",
-            help="Vendor to search for prefixes")
-    arg.add_option("-r","--random",
-            dest="rand",
-    action="store_true",
-            help="Generate random MAC")
-    arg.add_option("-a",
-            dest="stdall",
-            action="store_true",
-            help="Convert All From stdin")
+def build_parser():
+    from argparse import ArgumentParser
 
-    (options, args) = arg.parse_args()
-    if(options.download):
+    arg = ArgumentParser(prog=__name__)
+    arg.add_argument(
+        "-d",
+        "--download",
+        dest="download",
+        action="store_true",
+        help="Download latest MAC list",
+    )
+    arg.add_argument(
+        "-m", "--mac", dest="mac", help="MAC Address to Convert to Vendor String"
+    )
+    arg.add_argument(
+        "-v", "--vendor", dest="vendor", help="Vendor to search for prefixes"
+    )
+    arg.add_argument(
+        "-r", "--random", dest="rand", action="store_true", help="Generate random MAC"
+    )
+    arg.add_argument(
+        "-a", dest="stdall", action="store_true", help="Convert All From stdin"
+    )
+    return arg
+
+
+def main(argv):
+    arg = build_parser()
+    (options, args) = arg.parse_known_args(argv)
+    print((options, args))
+    if options.download:
         download_oui()
 
-    if(options.vendor and options.rand):
+    if options.vendor and options.rand:
         print(mac_from_vendor(options.vendor))
-    elif(options.vendor):
+    elif options.vendor:
         print(find_vendor(options.vendor))
 
-    if(options.mac and options.rand):
+    if options.mac and options.rand:
         print(mac_from_prefix(options.mac))
-    elif(options.mac):
+    elif options.mac:
         print(mac_to_vendor(options.mac))
 
-    if(options.stdall):
+    if options.stdall:
         ilines = sys.stdin.readlines()
         for x in ilines:
             print("%s -> %s" % (x.strip(), mac_to_vendor(x)))
+
+
+if __name__ == "__main__":
+    import sys
+    seed()
+    main(sys.argv)
