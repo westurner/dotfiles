c69e5f9560525c58d2ec3c1058696ff31e97bfa2
Wes Turner <wes@wrd.nu>
2026-04-03 18:22:32 -0400

    ENH: e: add `-l` and `set <name>` cli args to list editor paths and set editor defaults

--- scripts/_ewrd.sh
+++ scripts/_ewrd.sh
@@ -4,6 +4,103 @@
 #     # setup edit[*] and e[*] symlinks:
 #     $ ln -s ./_ewrd.sh _ewrd-setup.sh && ./_ewrd-setup.sh
 
+##    e, edit         -- edit a file, list editors, or set editor
+function e {
+    # e() -- Default editor wrapper with support for listing and setting editors.
+    # e -l/--list          -- List available editors and current settings
+    # e --set <name>       -- Print shell export command for setting $EDITOR
+    # e -h/--help          -- Show this help and editor's help
+    # e -- <file>          -- Edit a file (use -- to edit files named -l, set, etc.)
+    # e <file>             -- Edit file with $EDITOR (default: vim)
+    
+    POSSIBLE_EDITORS="code.sh code gvim nvim mvim vim nano vi emacs gedit kate spyder flatpak gvim-venv nvim-venv"
+
+    if [[ "$1" == "-l" || "$1" == "--list" ]]; then
+        echo "## Current settings:"
+        echo "  EDITOR=${EDITOR}"
+        echo "  EDITOR_=${EDITOR_}"
+        echo "  # TODO:"
+        echo "  VIMCONF=${VIMCONF}"
+        echo "  GUIVIMBIN=${GUIVIMBIN}"
+        echo "  GVIMBIN=${GVIMBIN}"
+        echo "  MVIMBIN=${MVIMBIN}"
+        echo ""
+        echo "## Available editors:"
+        echo ""
+        for ed in ${POSSIBLE_EDITORS}; do
+            if type "$ed" >/dev/null 2>&1; then
+                (set -x; type -a "$ed")
+            else
+                echo "+ type -a ${ed}"
+            fi
+            echo ""
+        done
+        return 0
+    elif [[ "$1" == "set" ]] || [[ "$1" == "-s" ]]; then
+        local editor_choice="$2"
+        echo "#xport EDITOR=\"$EDITOR\"  # current value of EDITOR"
+        case "$editor_choice" in
+            nano) echo "export EDITOR=\"nano\"" ;;
+
+            vi)   echo "export EDITOR=\"vi\"" ;;
+            vim)  echo "export EDITOR=\"vim\"" ;;
+            gvim) echo "export EDITOR=\"gvim\"" ;;
+            gvim-venv) echo "export EDITOR=\"gvim \${VIMCONF}\"" ;;
+            mvim) echo "export EDITOR=\"mvim\"" ;;
+            nvim) echo "export EDITOR=\"nvim\"" ;;
+            nvim-venv) echo "export EDITOR=\"nvim \${VIMCONF/--servername/--server}\"" ;;
+
+            gedit) echo "export EDITOR=\"gedit\"" ;;
+            kate) echo "export EDITOR=\"kate\"" ;;
+
+            spyder) echo "export EDITOR=\"spyder\"" ;;
+
+            code) echo "export EDITOR=\"code\"" ;;
+            code.sh) echo "export EDITOR=\"code.sh\"" ;;
+            com.visualstudio.code) echo "export EDITOR=\"flatpak run com.visualstudio.code\"" ;;
+            
+            org.gnome.TextEditor|TextEditor) echo "export EDITOR=\"flatpak run org.gnome.TextEditor\"" ;;
+
+
+            "")   echo "echo 'Error: Must specify editor name (e.g., code, vim)'" >&2; return 1 ;;
+            *)    echo "export EDITOR=\"$editor_choice\"" ;;
+        esac
+        return 0
+    elif [[ "$1" == "-h" || "$1" == "--help" ]]; then
+        # print function docstring
+        grep -E '^    # e' "${BASH_SOURCE[0]:-${__DOTFILES}/scripts/_ewrd.sh}" || true
+        echo ""
+        local _editor="${EDITOR:-vim}"
+        echo "Help for configured editor: $_editor"
+        (set -x; $_editor "$1")
+        return 0
+    fi
+
+    local _editor="${EDITOR:-vim}"
+    
+    if [[ "$1" == "--" ]]; then
+        shift
+    fi
+
+    # Expand default aliases if necessary, otherwise just run directly
+    $_editor "${@}"
+}
+
+function _e__complete {
+    local cur=${2}
+    local prev=${3}
+    if [[ "$prev" == "set" ]] || [[ "$prev" == "-s" ]]; then
+        COMPREPLY=( $(compgen -W "code.sh code com.visualstudio.code vim gvim gvim-venv nvim nvim-venv mvim vi nano gedit kate spyder org.gnome.TextEditor TextEditor" -- "$cur") )
+    else
+        COMPREPLY=( $(compgen -W "-l --list set -h --help --" -- "$cur") )
+        # Also include files/directories as fallback
+        COMPREPLY+=( $(compgen -f -- "$cur") )
+    fi
+}
+complete -o bashdefault -o default -o nospace -F _e__complete e
+complete -o bashdefault -o default -o nospace -F _e__complete edit
+
+
 ##    editdotfiles, edotfiles -- cd $__DOTFILES and run edit w/ each arg
 function editdotfiles {
     # editdotfiles() -- cd $__DOTFILES and run edit w/ each arg
@@ -205,6 +302,9 @@ complete -o default -o nospace -F _ewww__complete ewww
 function _create_ewrd_symlinks {
     local scriptname='_ewrd.sh'
     local scriptnames=(
+        "e"
+        "edit"
+
         "editdotfiles"
         "edotfiles"
 
@@ -242,8 +342,13 @@ function _create_ewrd_symlinks {
 #_ewrd.sh main()
 if [ -n "${BASH_SOURCE}" ] && [ "${BASH_SOURCE}" == "${0}" ]; then
     set -x
-    declare -r progname="$(basename ${BASH_SOURCE})"
+    declare -r progname="$(basename "${BASH_SOURCE}")"
     case "${progname}" in
+        e|edit)
+            e "${@}"
+            exit
+            ;;
+
         editdotfiles|edotfiles)
             editdotfiles "${@}"
             exit
