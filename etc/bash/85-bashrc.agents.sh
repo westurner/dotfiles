d33677622da0f11c562951a0c5bcd5f0f946c72e
Wes Turner <wes@wrd.nu>
2026-06-01 07:47:31 +0000

    PRO,ETC: 85-bashrc.agents.sh: improve virtual environment prompt handling in VSCode

--- etc/bash/85-bashrc.agents.sh
+++ etc/bash/85-bashrc.agents.sh
@@ -1,9 +1,35 @@
-
+#!/bin/bash
 ## 85-bashrc.agents.sh
 
+remove_venvprompt_prefix() {
+
+    ps1_extra_prefix="\[\](dotfiles) \[\]${VENVPROMPT}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\n\$ \[\]\[\]"
+    ps1_expected="\[\]${VENVPROMPT}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\n\$ \[\]\[\]"
+
+    ps1="${ps1_extra_prefix}"
+
+    if [ ! "${ps1}" = "${ps1_expected}" ]; then
+        printf "ERROR: ps1 != ps2 \n"
+        printf "ps1=%s\n" "${ps1}"
+        printf "pse=%s\n" "${ps1_expected}"
+    fi
+
+    unset ps1_extra_prefix ps1_expected ps1
+}
+
 if [[ "$TERM_PROGRAM" == "vscode" ]]; then
-    source "$(code --locate-shell-integration-path bash)"
+    echo "INFO: bashrc.agents.sh Running in vscode"
+    vscode_shell_integration_sh="$(code --locate-shell-integration-path bash)"
+    source "${vscode_shell_integration_sh}"
     export GIT_PAGER=
+    export EDITOR=code
+
+    if [ -n "${VIRTUAL_ENV}" ]; then
+        workon "${VIRTUAL_ENV}"
+    else
+        remove_venvprompt_prefix
+    fi
+
 fi
 
 export __AGENT=1
